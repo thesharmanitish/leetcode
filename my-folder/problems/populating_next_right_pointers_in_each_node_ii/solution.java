38d4f0b35277d641915493e9c581b8e15adcb8c8
Nitish Sharma <nitss007@gmail.com>
2022-01-03 13:29:27 +0000

    Sync LeetCode submission - Populating Next Right Pointers in Each Node II (java)

---
+++ my-folder/problems/populating_next_right_pointers_in_each_node_ii/solution.java
@@ -0,0 +1,100 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+
+class Solution {
+/*    public Node connect(Node root) {
+        Deque<Node> bfs = new LinkedList<>();
+        bfs.add(root);
+        Deque<Node> layer = new LinkedList<>();
+        while(!bfs.isEmpty()){
+            while(!bfs.isEmpty())
+            layer.offerLast(bfs.pop());
+               
+        Node prev_node =  layer.pop();
+        if(prev_node == null ) return root;
+        if(prev_node.left !=null) bfs.offerLast(prev_node.left);
+        if(prev_node.right !=null) bfs.offerLast(prev_node.right);            
+        int size  = layer.size();
+         for(int i=0;i<size;i++){
+            Node node = layer.pop();
+             if(node!=null) 
+                 prev_node.next = node;
+             prev_node = node;
+            if(node.left !=null) bfs.offerLast(node.left);
+            if(node.right !=null) bfs.offerLast(node.right);
+        }
+      }
+        return root;
+    }
+    
+    public Node connect(Node root) {
+        Node cur = root, levelHead = new Node(0);
+        
+        while(cur!=null){
+            Node needle = levelHead;
+            while(cur!=null){
+                if(cur.left!=null){
+                    needle.next = cur.left;
+                    needle = needle.next;
+                }
+                if(cur.right!=null){
+                    needle.next = cur.right;
+                    needle = needle.next;
+                }
+                cur = cur.next;
+            }
+            cur = levelHead.next;
+            levelHead.next = null;
+        }
+        return root;
+    }
+    */
+        
+    public Node connect(Node root) {
+        if(root ==null) return root;
+        Node cur = root, levelHead = new Node(0);
+        
+            Node needle = levelHead;
+            while(cur!=null){
+                if(cur.left!=null){
+                    needle.next = cur.left;
+                    needle = needle.next;
+                }
+                if(cur.right!=null){
+                    needle.next = cur.right;
+                    needle = needle.next;
+                }
+                if(cur.next!=null)
+                    cur = cur.next;
+                else{
+                    cur = levelHead.next;
+                    levelHead.next = null;
+                    needle = levelHead;
+                }
+            }
+            cur = levelHead.next;
+            levelHead.next = null;
+        
+        return root;
+    }
+}
