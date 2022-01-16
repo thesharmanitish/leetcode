d5fe5c79485e0f75a8ee97a8dc5d3266f556ed0c
Nitish Sharma <nitss007@gmail.com>
2022-01-16 19:54:11 +0000

    Sync LeetCode submission - Course Schedule (java)

---
+++ my-folder/problems/course_schedule/solution.java
@@ -0,0 +1,43 @@
+class Solution {
+    public boolean canFinish(int n, int[][] prerequisites) {
+//         if(prerequisites.length==0) return true;
+//         boolean[][] grid = new boolean[numCourses][numCourses];
+//         int[] indegree = new int[numCourses];
+        
+//         for(int i=0;i<prerequisites.length;i++){
+//           grid[prerequisites[i][0]][prerequisites[i][1]] = true;
+//           indegree[prerequisites[i][1]]++;  
+//         }
+        
+//         Deque<Integer> bfs = new LinkedList<>();
+//         for(int i=0;i<numCourses;i++){
+//             if(indegree[i]==0)
+//                bfs.offer(i) ;
+//         }
+//         int count = 0;
+//         while(!bfs.isEmpty()){
+//             count++;
+//             int num = bfs.pop();
+//             for(int i=0;i<numCourses;i++){
+//                 if(grid[num][i] && --indegree[i]==0){
+//                     bfs.offer(i) ;
+//                 }
+//             }
+//         }
+//         return count==numCourses;
+               ArrayList<Integer>[] G = new ArrayList[n];
+        int[] degree = new int[n];
+        ArrayList<Integer> bfs = new ArrayList();
+        for (int i = 0; i < n; ++i) G[i] = new ArrayList<Integer>();
+        for (int[] e : prerequisites) {
+            G[e[1]].add(e[0]);
+            degree[e[0]]++;
+        }
+        for (int i = 0; i < n; ++i) if (degree[i] == 0) bfs.add(i);
+        for (int i = 0; i < bfs.size(); ++i)
+            for (int j: G[bfs.get(i)])
+                if (--degree[j] == 0) bfs.add(j);
+        return bfs.size() == n;
+    }
+
+}
