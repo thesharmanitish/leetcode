76e98cf491322b59e00f3e34d9ebb1073c6c794c
Nitish Sharma <nitss007@gmail.com>
2022-02-12 18:32:56 +0000

    Sync LeetCode submission - Binary Tree Level Order Traversal (java)

--- my-folder/problems/binary_tree_level_order_traversal/solution.java
+++ my-folder/problems/binary_tree_level_order_traversal/solution.java
@@ -15,26 +15,24 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> traverse = new ArrayList();
-        Deque<TreeNode> queue = new LinkedList();
-        Deque<TreeNode> level = new LinkedList();
-        if(root == null) return traverse;
-        queue.add(root);
-        while(!queue.isEmpty()){
-            while(!queue.isEmpty())
-                level.add(queue.poll());
-            List<Integer> levelArr = new ArrayList();
-            while(!level.isEmpty()){
-                TreeNode curNode = level.poll();
-                if(curNode!=null){
-                    levelArr.add(curNode.val);
-                    if(curNode.left != null) queue.add(curNode.left);
-                    if(curNode.right != null) queue.add(curNode.right);
-             }
+        
+        Deque<TreeNode> bfs = new LinkedList<>();
+        List<List<Integer>> res = new LinkedList<>();
+        if(root == null) return res;
+        bfs.addLast(root);
+        while(!bfs.isEmpty()){
+            int size = bfs.size();
+            List<Integer> l = new LinkedList<>();
+            for(int i=0;i<size;i++){
+                TreeNode t = bfs.pollFirst();
+                l.add(t.val);
+                if(t.left !=null) bfs.addLast(t.left);
+                if(t.right !=null) bfs.addLast(t.right);
+                
             }
+            res.add(l);
             
-            traverse.add(levelArr);
         }
-    return traverse;
+        return res;
     }
 }
