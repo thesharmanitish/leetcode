ca5ad6c43db1bb7a387a54ecbd00d066a5c6ce1c
Nitish Sharma <nitss007@gmail.com>
2022-02-12 18:47:18 +0000

    Sync LeetCode submission - Symmetric Tree (java)

--- my-folder/problems/symmetric_tree/solution.java
+++ my-folder/problems/symmetric_tree/solution.java
@@ -12,43 +12,19 @@
  *         this.right = right;
  *     }
  * }
- 
+ */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if(root ==null) return true;
-        return  isSymmetric(root.left, root.right);
+        if(root==null ) return true;
+        return isSymmetric(root.left,root.right) ;
     }
-        public boolean isSymmetric(TreeNode left, TreeNode right) {
-        if(left == null && right == null) return true;
-        else if(left == null || right == null || left.val != right.val) return false;
-        else return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
-            
-    }
-    
-    
-    
-}
-*/
-
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if(root==null) return true;
-        
-        Deque<TreeNode> stack  = new LinkedList();
-        stack.add(root.left);
-        stack.add(root.right);
-        while(!stack.isEmpty()){
-            TreeNode left = stack.removeFirst();
-            TreeNode right = stack.removeFirst();
-            
-            if(left == null && right ==null) continue;
-            else if(left == null || right == null || left.val != right.val) return false;
-            else{
-                stack.add(left.left); stack.add(right.right);
-                stack.add(left.right); stack.add(right.left);
-            }
-            
+    public boolean isSymmetric(TreeNode left, TreeNode right) {
+        if(left ==null || right==null){
+            return left == right;
         }
-        return true;
+        else if(left.val == right.val){
+            return isSymmetric(left.left,right.right) && isSymmetric(left.right,right.left) ; 
+        }
+        return false;
     }
 }
