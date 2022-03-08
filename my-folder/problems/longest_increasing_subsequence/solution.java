0b423c48eeb72711a5d60cbdd175b0a44338df26
Nitish Sharma <nitss007@gmail.com>
2022-03-08 13:43:18 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- my-folder/problems/longest_increasing_subsequence/solution.java
+++ my-folder/problems/longest_increasing_subsequence/solution.java
@@ -1,30 +1,32 @@
 class Solution {
-    public int lengthOfLIS(int[] graph) {
+    public int lengthOfLIS(int[] nums) {
         
-       
-		int size = graph.length;
-         if(size<2) return size;
-		int count[] = new int[size];
-		int lastIndex[] = new int[size];
-		
-		for (int i = 0; i < lastIndex.length; i++) {
-			count[i] = 1;
-		}
-		for (int i = 1; i < graph.length; i++) {
-			for (int j = 0; i>j; j++) {
-				if(graph[j]<graph[i] && count[j]+1 >= count[i]){
-					count[i] = count[j]+1;
-					lastIndex[i] = graph[j];
-				}
-			}
-		}
-		int max = Integer.MIN_VALUE;
-		for (int i = 0; i < count.length; i++) {
-			System.out.print(count[i] + " : ");
-
-			if(max<count[i]) max = count[i];
-		}
-        return max;
-	
+        int[] tails = new int[nums.length];
+	    int size = 0;
+	    for(int num:nums) {
+	    	int i=0, j= size;
+	    	while(i<j) {
+	    		int mid = (i+j)/2;
+	    		if(num>tails[mid]) {
+	    			i = mid+1;
+	    		}else {
+	    			j= mid;
+	    		}
+	    	}
+	    	if(i==size) size++;
+	    	tails[i] = num;
+	    
+	    }
+	    return size;
+        
+        // TreeSet<Integer> tset = new TreeSet<>();
+        // for(int i=0;i<nums.length;i++){
+        //     Integer x = tset.ceiling(nums[i]);
+        //     if(x !=null){
+        //         tset.remove(x);
+        //     }
+        //     tset.add(nums[i]);
+        // }
+        // return tset.size();
     }
 }
