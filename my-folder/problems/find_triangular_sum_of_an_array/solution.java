fb81156621176dc5d8511c0d6fc49cbe83e6383a
Nitish Sharma <nitss007@gmail.com>
2022-04-03 06:51:45 +0000

    Sync LeetCode submission - Find Triangular Sum of an Array (java)

---
+++ my-folder/problems/find_triangular_sum_of_an_array/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        int size = nums.length;
+        while(size>0){
+            for(int i=0;i<size-1;i++){
+                nums[i] = (nums[i] +nums[i+1])%10;
+
+
+            }
+            size--;
+        }
+        return nums[0];
+    }
+}
