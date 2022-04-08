649f257a8ff15a9858e8b7df71abf3380be982a7
Nitish Sharma <nitss007@gmail.com>
2022-04-08 18:41:56 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock IV (java)

--- my-folder/problems/best_time_to_buy_and_sell_stock_iv/solution.java
+++ my-folder/problems/best_time_to_buy_and_sell_stock_iv/solution.java
@@ -10,8 +10,8 @@ class Solution {
             max = -prices[0];
 			for (int j = 1; j < T[0].length; j++) {
                 
-                T[i][j] = Math.max(max+prices[j], T[i][j-1]);
-                max= Math.max(max,-prices[j]+ T[i-1][j]);
+                T[i][j] = Math.max(max+prices[j], T[i][j-1]); // sold on this day , max profit today or previous day
+                max= Math.max(max,-prices[j]+ T[i-1][j]);  // max update to max of total profit today or previous transaction
 			}
 		}
 		return T[K][prices.length - 1];
