5d835a55fae4fcbd2865ba561fcf6a34a5606cca
Nitish Sharma <nitss007@gmail.com>
2022-04-09 07:28:13 +0000

    Sync LeetCode submission - Maximum Number of Visible Points (java)

--- my-folder/problems/maximum_number_of_visible_points/solution.java
+++ my-folder/problems/maximum_number_of_visible_points/solution.java
@@ -1,31 +1,32 @@
 class Solution {
     public int visiblePoints(List<List<Integer>> points, int angle, List<Integer> location) {
         int count = 0;
-        List<Double> anglesList = new ArrayList<>();
+        List<Double> angles = new ArrayList<>();
         for(List<Integer> p:points){
-            int dx = p.get(0)-location.get(0);
-            int dy = p.get(1)-location.get(1);
-            if(dx ==0 && dy ==0){
+            int dx = p.get(0) - location.get(0) ;
+            int dy = p.get(1) - location.get(1) ;
+            if(dx==0 && dy ==0){
                 count++;
-                continue;
-            }
-                
-            anglesList.add(Math.atan2(dy,dx)*(180 / Math.PI));
+                continue;}
+            double a = Math.atan2(dy,dx)*(180/Math.PI);
+                                   
+            angles.add(a);
+                                   
         }
-        Collections.sort(anglesList);
-        int k=0;
-        List<Double> tmp = new ArrayList<>(anglesList);
-        for (double d : anglesList) 
-            tmp.add(d + 360); // concatenate to handle edge case
-        int res = count;
-        for (int i = 0, j = 0; i < tmp.size(); i++) {
-            while (tmp.get(i) - tmp.get(j) > angle) {
+                                   
+        Collections.sort(angles);
+        List<Double> temp = new ArrayList<>(angles);
+        for(double t:temp){
+            angles.add(t+360);
+        }
+        int res = 0;
+        for(int i=0,j=0;i<angles.size();i++){
+            while(angles.get(i)-angles.get(j)>angle){
                 j++;
             }
-            res = Math.max(res, count + i - j + 1);
+            res = Math.max(res, i-j+1);
         }
-        return res;
-    
-        
+                                   
+        return res+count;
     }
 }
