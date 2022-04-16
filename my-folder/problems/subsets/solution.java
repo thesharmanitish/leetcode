6c7e1d45e0e8d0a95e208dad1b8c58cd5f8e7e58
Nitish Sharma <nitss007@gmail.com>
2022-04-16 14:18:00 +0000

    Sync LeetCode submission - Subsets (java)

--- my-folder/problems/subsets/solution.java
+++ my-folder/problems/subsets/solution.java
@@ -1,7 +1,24 @@
 class Solution {
+    
+// 	public List<List<Integer>> subsets(int[] S) {
+// 		List<List<Integer>>  res = new ArrayList<>();
+// 		res.add(new ArrayList<>());
+// 		for(int num:S) {
+// 			List<List<Integer>>  t = new ArrayList<>();
+// 			for(int i=0;i<res.size();i++) {
+// 				List<Integer> tmp = new ArrayList<>(res.get(i));
+// 				tmp.add(num);
+// 				t.add(tmp);
+// 			}
+// 			res.addAll(t);
+// 		}
+// 		return res;
+		
+// 	}
+    
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
+        // Arrays.sort(nums);
         backtrack(nums, res, new ArrayList<>(), 0);
         return res;
     }
