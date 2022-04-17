aeccc51f0cd587e2f5ae012b961cb7cf4400359e
Nitish Sharma <nitss007@gmail.com>
2022-04-17 05:49:19 +0000

    Sync LeetCode submission - Max Consecutive Ones (java)

---
+++ my-folder/problems/max_consecutive_ones/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+     int start =0 , end = 0, max =0,res =0;
+     for(int i:nums){
+         if(i==0){
+             start=end+1;
+         }
+         
+         max = end-start+1;
+         if(max>res)
+             res= max;
+         end++;
+        }
+        return res;
+    }
+}
