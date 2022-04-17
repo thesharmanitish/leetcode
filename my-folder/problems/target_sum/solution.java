60ac6ee8ed7228fef2661b994d00eeea06d8ac6a
Nitish Sharma <nitss007@gmail.com>
2022-04-17 15:24:11 +0000

    Sync LeetCode submission - Target Sum (java)

--- my-folder/problems/target_sum/solution.java
+++ my-folder/problems/target_sum/solution.java
@@ -1,44 +1,27 @@
 class Solution {
-    public int findTargetSumWays(int[] nums, int S) {
-        
-        int sum = 0;
-        for (int n: nums) sum += n;
-        if (sum < S || (sum - S) % 2 == 1) return 0;
-        int target = (sum - S) / 2;
-        return findTargetSumWays(nums,nums.length,target);
-        // int[] dp = new int[target + 1];
-        // dp[0] = 1;
-        // for (int i = 0; i < nums.length; i++) {
-        //     for (int j = target; j >= nums[i]; j--) {
-        //     dp[j] += dp[j - nums[i]];
-        //     }
-        // }
-        // return dp[target];
-        
-    }
-    int[][] dp;
-    public int findTargetSumWays(int[] a,int n, int sum) {
-  // Initializing the matrix
-    dp = new int[n + 1][sum + 1];
-  // Initializing the first value of matrix
-    dp[0][0] = 1;
-    for (int i = 1; i <= sum; i++)
-        dp[0][i] = 0;
-    // for (int i = 1; i <= n; i++)
-    //     dp[i][0] = 1;
- 
-    for (int i = 1; i <= n; i++)
-    {
-        for (int j = sum; j >=0; j--)  // this 1 change in subset sum problem
-        {
-            if (a[i - 1] <= j)
-                dp[i][j] = dp[i - 1][j] + dp[i - 1][j - a[i - 1]]; 
-            else
-           dp[i][j] = dp[i - 1][j];
-        }
-    }
+    public int findTargetSumWays(int[] nums, int target) {
 
-    return dp[n][sum];
+
+		int sum = Arrays.stream(nums).sum();
+        if(((sum - target) % 2 == 1) || sum<target)
+            return 0;
+        int b = (sum -target )/2;
+		int n = nums.length;
+		int[][] dp = new int[n+1][b + 1];
         
+        for(int i=0;i<=n;i++)
+            dp[i][0] = 1;
+		for (int i=1;i<=n;i++) {
+            for(int j=0;j<=b;j++){
+                if( nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j]+dp[i-1][j-nums[i-1]];
+                }
+                else 
+                    dp[i][j] = dp[i-1][j];
+            }
+		}
+
+		return dp[n][b];
+	        
     }
 }
