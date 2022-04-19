8c5acdbbbda2db60394c8c189f8de0c0584a75c7
Nitish Sharma <nitss007@gmail.com>
2022-04-19 06:06:22 +0000

    Sync LeetCode submission - Pow(x, n) (java)

--- my-folder/problems/pow(x,_n)/solution.java
+++ my-folder/problems/pow(x,_n)/solution.java
@@ -1,25 +1,23 @@
 class Solution {
     public double myPow(double x, int n) {
-        double res =1;
-        if(x == 0)
+        if(x ==0 || n==1)
             return x;
-        else if(x==1 || n==0)
+        else if(n==0)
             return 1;
-        int t = n;
-        if(n<0){
-            x = 1/x;
-            n = -(n+1);//for Integer.MIN_VALUE
-            res = res*x;
-            }
-        while(n>0){
-            if((n&1)==1){
-                res = res*x;
-            }
-            x = x*x;
-            n= n>>1;
-        }
-
-        return res;
         
+        double res=1;
+        if(n<0){
+            x= (1/x);
+            n = -(n+1);
+            res = res*x;
+        }
+        
+        while(n>0){
+            if((n&1)==1)
+                res *=x;
+            x *= x;
+            n >>=1;
+        }
+        return res;
     }
 }
