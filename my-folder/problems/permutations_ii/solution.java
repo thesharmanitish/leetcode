76ad56921050eeb8903644c26afc7648dd8d2d6c
Nitish Sharma <nitss007@gmail.com>
2022-05-12 12:47:14 +0000

    Sync LeetCode submission - Permutations II (java)

--- my-folder/problems/permutations_ii/solution.java
+++ my-folder/problems/permutations_ii/solution.java
@@ -1,92 +1,28 @@
 class Solution {
-//     public List<List<Integer>> permuteUnique(int[] nums) {
-//         List<List<Integer>> res = new ArrayList<>();
-        
-//         permute(nums, res, new ArrayList<Integer>(), 0);
-//         return res;
-//     }
-
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-//    Map<String, String> set = new HashMap<>();    
-//    public void permute(int[] nums, List<List<Integer>> res,List<Integer> suffix, int ind) {
-//         if(suffix.size()==nums.length){
-//             StringBuilder sb = new StringBuilder();
-//             for(Integer i:suffix)
-//                 sb.append(i);
-//             // String s = suffix.stream().map(key -> key+"")
-//             //         .collect(Collectors.joining(", "));
-//             if(set.get(sb.toString())==null){
-//                 set.put(sb.toString(),"1");
-//                 res.add(suffix);
-//             }
-//             return;
-//         }
-//        int num = nums[ind];
-//        for(int i=0;i<=suffix.size();i++){
-//            List<Integer> ll = new ArrayList<>(suffix);
-//            ll.add(i,num);
-//            permute(nums, res, ll, ind+1)    ;
-//        }
-//     }
-    
-    
     public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<List<Integer>>();
-        if(nums==null || nums.length==0) return res;
-        boolean[] used = new boolean[nums.length];
-        List<Integer> list = new ArrayList<Integer>();
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-        dfs(nums, used, list, res);
-        return res;
+        boolean[] used = new boolean[nums.length];
+        permuteUnique(nums, used, result,new ArrayList<>());
+        return result;
     }
-
-    public void dfs(int[] nums, boolean[] used, List<Integer> list, List<List<Integer>> res){
-        if(list.size()==nums.length){
-            res.add(new ArrayList<Integer>(list));
-            return;
+    
+    public void permuteUnique(int[] nums, boolean[] used , List<List<Integer>> result, List<Integer> current) {
+        if(current.size() ==nums.length){
+            result.add(new ArrayList<Integer>(current));
+            return ;
         }
-        for(int i=0;i<nums.length;i++){
-            if(used[i]) continue;
-            if(i>0 &&nums[i-1]==nums[i] && !used[i-1]) continue;  //select only those when i-1 is present
-            used[i]=true;
-            list.add(nums[i]);
-            dfs(nums,used,list,res);
-            used[i]=false;
-            list.remove(list.size()-1);
+        
+        for(int ind=0;ind<nums.length;ind++){
+            if(used[ind])
+                continue;
+            if(ind>0 && nums[ind] == nums[ind-1] && !used[ind-1])
+                continue;
+            used[ind] = true;
+            current.add(nums[ind]);
+            permuteUnique(nums, used, result,current);
+            used[ind] = false;
+            current.remove(current.size()-1);
         }
-   }
+    }    
 }
