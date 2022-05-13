8a761f1a39a1801077da4ca18acb647f4f4a0b28
Nitish Sharma <nitss007@gmail.com>
2022-05-13 07:37:25 +0000

    Sync LeetCode submission - Contains Duplicate III (java)

--- my-folder/problems/contains_duplicate_iii/solution.java
+++ my-folder/problems/contains_duplicate_iii/solution.java
@@ -1,5 +1,30 @@
 class Solution {
-    // public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) { 
+    
+    
+     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) { 
+         
+         Map<Long, Long> map = new HashMap<>();
+         
+         for(int i=0;i<nums.length;i++){
+             long temp = (long) nums[i] - Integer.MIN_VALUE;
+             long bucket = temp/((long)t+1);
+             
+             if(map.containsKey(bucket) ||
+               (map.containsKey(bucket-1) && temp <= map.get(bucket-1)+t )||
+               (map.containsKey(bucket+1) && map.get(bucket+1)<= temp +t ))
+                 return true;
+             map.put(bucket, temp);
+             
+             if(map.size()>k){
+                 long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+                 map.remove(lastBucket);
+             }            
+         }
+       return false;
+     }
+// Bucket approch
+    
+//     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) { 
 //         if (k < 1 || t < 0) return false;
 //         Map<Long, Long> map = new HashMap<>();
         
@@ -20,22 +45,22 @@ class Solution {
 //         return false;
 //     }
 
-    public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) { 
-        TreeSet<Integer> treeset = new TreeSet<>();
+//     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) { 
+//         TreeSet<Integer> treeset = new TreeSet<>();
         
-        for(int i=0;i<nums.length;i++){
-            if(treeset.ceiling(nums[i])!=null && (long) treeset.ceiling(nums[i]) <= nums[i]+ t)
-                return true;
-            if(treeset.floor(nums[i])!=null && nums[i] <= (long) treeset.floor(nums[i])+ t)
-                return true;
-            treeset.add(nums[i]);      
-            if(treeset.size() > k)
-                treeset.remove(nums[i-k]);
+//         for(int i=0;i<nums.length;i++){
+//             if(treeset.ceiling(nums[i])!=null && (long) treeset.ceiling(nums[i]) <= nums[i]+ t)
+//                 return true;
+//             if(treeset.floor(nums[i])!=null && nums[i] <= (long) treeset.floor(nums[i])+ t)
+//                 return true;
+//             treeset.add(nums[i]);      
+//             if(treeset.size() > k)
+//                 treeset.remove(nums[i-k]);
             
   
             
-        }
-        return false;
+//         }
+//         return false;
         
-    }
+//     }
 }
