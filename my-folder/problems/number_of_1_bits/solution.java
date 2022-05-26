9428d1aec7dcd2eda16e7a8d431ea86f79823b9c
Nitish Sharma <nitss007@gmail.com>
2022-05-26 18:08:32 +0000

    Sync LeetCode submission - Number of 1 Bits (java)

--- my-folder/problems/number_of_1_bits/solution.java
+++ my-folder/problems/number_of_1_bits/solution.java
@@ -2,9 +2,10 @@ public class Solution {
     // you need to treat n as an unsigned value
     public int hammingWeight(int n) {
         int x = 0;
-        for(int i=0;i<32;i++){
-            x += (n&1)==1 ? 1:0;
-            n = n>>1;
+
+        while(n!=0){
+            x ++;
+            n &= (n - 1);
             
         }
         return x;        
