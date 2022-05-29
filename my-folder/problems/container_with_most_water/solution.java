f8651ff2e79ededea86bcacd0e677f3b45c67796
Nitish Sharma <nitss007@gmail.com>
2022-05-29 19:35:05 +0000

    Sync LeetCode submission - Container With Most Water (java)

--- my-folder/problems/container_with_most_water/solution.java
+++ my-folder/problems/container_with_most_water/solution.java
@@ -1,20 +1,17 @@
 class Solution {
     public int maxArea(int[] height) {
-        int max = 0, start = 0, end = height.length-1, minpiller = 0, res = Integer.MIN_VALUE;
-        while(start<end){
-            if(height[start]<height[end]){
-              minpiller = start;
-              max = height[minpiller]*(end-start);
-              start++;  
-                
-            }else{
-                minpiller = end;
-                max = height[minpiller]*(end-start);
+		int start = 0, end = height.length - 1, max = 0, mul = 0;
+		while (start < end) {
+            mul = Math.min(height[start], height[end]) * (end - start);
+			if (height[start] < height[end]) {
+				start++;
+			} else {
                 end--;
-            }
-            
-            res = Math.max(res, max);
-        }
-        return res;
+			}
+			max = Math.max(max, mul);
+
+		}
+        return max;
     }
+
 }
