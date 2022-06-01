8d38d649b49f9f38cad9f48783a0e5ee634bf62d
Nitish Sharma <nitss007@gmail.com>
2022-06-01 06:30:50 +0000

    Sync LeetCode submission - Determine Whether Matrix Can Be Obtained By Rotation (java)

--- my-folder/problems/determine_whether_matrix_can_be_obtained_by_rotation/solution.java
+++ my-folder/problems/determine_whether_matrix_can_be_obtained_by_rotation/solution.java
@@ -1,18 +1,21 @@
 class Solution {
     public boolean findRotation(int[][] mat, int[][] target) {
         for(int i=0;i<4;i++){
-            if(Arrays.deepEquals(mat, target))
+            if(Arrays.deepEquals(mat,target))
                 return true;
             rotate(mat);
         }
+        
         return false;
     }
     public void rotate(int[][] mat) {
-        for (int i = 0, j = mat.length - 1; i < j; ++i, --j) { // reverse rows order.
-            int[] tmp = mat[i];
+        
+    for (int i = 0, j = mat.length - 1; i < j; ++i, --j){
+            int[] temp = mat[i];
             mat[i] = mat[j];
-            mat[j] = tmp;
-        } 
+            mat[j] = temp;
+            
+        }
         
         for(int i=0;i<mat.length;i++){
             for(int j=i+1;j<mat[0].length;j++){
