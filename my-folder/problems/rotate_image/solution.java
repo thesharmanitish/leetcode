676c32404f2dbe3b5c8c2455690f251aa617073e
Nitish Sharma <nitss007@gmail.com>
2022-06-02 08:27:52 +0000

    Sync LeetCode submission - Rotate Image (java)

--- my-folder/problems/rotate_image/solution.java
+++ my-folder/problems/rotate_image/solution.java
@@ -1,31 +1,16 @@
 class Solution {
-    // public void rotate(int[][] matrix) {
-    //     int n= matrix.length;
-    //     for(int i=0;i<n/2;i++){
-    //         for(int j=i;j<n-1-i;j++){
-    //             // System.out.print(matrix[row][col]);
-    //             swap(matrix, i,j,j,n-i-1 );
-    //             swap(matrix, i,j,n-i-1,n-j-1 );
-    //             swap(matrix, i,j,n-j-1,i );
-    //             // int tmp = matrix[i][j];
-    //             // matrix[i][j] = matrix[n-j-1][i];
-    //             // matrix[n-j-1][i]=matrix[n-i-1][n-j-1];
-    //             // matrix[n-i-1][n-j-1]=matrix[j][n-i-1];
-    //             // matrix[j][n-i-1]=tmp;                
-    //         }
-    //     }
-    // }
-    public void rotate(int[][] mat) {
-        for (int i = 0, j = mat.length - 1; i < j; ++i, --j) { // reverse rows order.
-            int[] tmp = mat[i];
-            mat[i] = mat[j];
-            mat[j] = tmp;
-        } 
-        for (int r = 0, R = mat.length; r < R; ++r) { // transpose.
-            for (int c = r + 1; c < R; ++c) {
-                int tmp = mat[r][c];
-                mat[r][c] = mat[c][r];
-                mat[c][r] = tmp;
+    public void rotate(int[][] matrix) {
+        
+        for(int i =0;i<matrix.length/2;i++){
+            int[] temp = matrix[i];
+            matrix[i] = matrix[matrix.length-i-1];
+            matrix[matrix.length-i-1] = temp;
+        }
+        for(int i=0;i<matrix.length;i++){
+            for(int j=i+1;j<matrix[0].length;j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
     }
