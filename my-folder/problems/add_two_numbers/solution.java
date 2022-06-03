366e0cf78f27f5ff290e23c672442013116fa0dd
Nitish Sharma <nitss007@gmail.com>
2022-06-03 10:35:57 +0000

    Sync LeetCode submission - Add Two Numbers (java)

--- my-folder/problems/add_two_numbers/solution.java
+++ my-folder/problems/add_two_numbers/solution.java
@@ -10,44 +10,46 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head1 = l1, head2 = l2, prev=null;
-        int carry = 0;
-        
-        while(l1 !=null && l2 !=null){
-            int sum = l1.val+l2.val +carry;
-            l1.val = sum%10;
-            carry = sum/10;
+        ListNode head = l1, prev = null;
+        int carry =0;
+        while(l1 != null && l2 != null){
+            int sum = l1.val+l2.val+carry;
+            l1.val = sum>9?sum-10:sum;
+            carry = sum>9?1:0;
             prev = l1;
             l1 = l1.next;
             l2 = l2.next;
         }
-        if(l2 !=null){
-            l1 = prev;          
-            l1.next = l2;
-            l1 = l1.next;  
-            while(l1 !=null){
-                int sum = l1.val +carry;
+        if(l2!=null){
+           l1 = prev; 
+           l1.next = l2;
+           l1 = l1.next;
+           addTillEnd(l1, carry);
+            
+        }
+        else if(l1 != null){
+            addTillEnd(l1, carry);   
 
-                l1.val = sum%10;
-                carry = sum/10;
+        }else{
+            if(carry>0)
+               prev.next = new ListNode(carry);
+        }
+        
+        return head;
+    }
+    public void addTillEnd(ListNode l1, int carry) {
+            ListNode prev = null;
+            while(l1 !=null){
+                int sum = l1.val + carry;
+                l1.val = sum>9?sum-10:sum;
+                carry = sum>9?1:0;  
                 prev = l1;
                 l1 = l1.next;
             }
-        }else if(l1!=null){
-            while(l1 !=null){
-                int sum = l1.val +carry;
-
-                l1.val = sum%10;
-                carry = sum/10;
-                prev = l1;
-                l1 = l1.next;
-            }            
-        }
-        if(carry>0){
-            ListNode t = new ListNode(carry);
-            prev.next = t;
-        }
+        if(carry>0)
+            prev.next = new ListNode(carry);
+        
+        
             
-        return head1;
     }
 }
