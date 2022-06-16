25daba2c9edae1a9750bff40b8ba8957942dab28
Nitish Sharma <nitss007@gmail.com>
2022-06-16 06:10:02 +0000

    Sync LeetCode submission - Longest Palindromic Substring (java)

--- my-folder/problems/longest_palindromic_substring/solution.java
+++ my-folder/problems/longest_palindromic_substring/solution.java
@@ -1,10 +1,9 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String res ="";
         if(s.length()==0)
             return s;
         char[] arr= s.toCharArray();
-        int n = s.length()-1, start= 0, end= 0;
+        int n = arr.length -1 , start = 0, end= 0;
         int i=n, j=0;
         for(i=n, j=0 ; i>j;i--,j++){
             if(arr[i] != arr[j]){
@@ -16,14 +15,15 @@ class Solution {
 
        
        boolean[][] dp  = new boolean[n+1][n+1];
-       for(i=n;i>=0;i--) {
-           for(j=i;j<=n;j++){
-               if(arr[i] == arr[j] && (j<i+2 || dp[i+1][j-1])){
+       for( i=n;i>=0;i--){
+           for( j=i;j<=n;j++){
+               if(arr[i] == arr[j] && (j-i<2 || dp[i+1][j-1])){
                    dp[i][j] = true;
-                   if(end-start < j-i || res == null){
-                           start = i;
-                           end = j;
+                   if(end-start<j-i){
+                       end =j;
+                       start =i;
                    }
+                       
                }
            }
        }
