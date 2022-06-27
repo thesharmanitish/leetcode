7f0c9b1a1c0b4512324eb5592ff9887e7c294861
Nitish Sharma <nitss007@gmail.com>
2022-06-27 15:14:29 +0000

    Sync LeetCode submission - Scramble String (java)

--- my-folder/problems/scramble_string/solution.java
+++ my-folder/problems/scramble_string/solution.java
@@ -11,6 +11,17 @@ class Solution {
         if(map.containsKey(s1+"_"+s2))
             return map.get(s1+"_"+s2);
         int n = s1.length();
+
+        int s1Array[] = new int[26];
+        int s2Array[] = new int[26];
+        for(int i = 0; i < s1.length(); i++) {
+            s1Array[s1.charAt(i) - 'a']++;
+            s2Array[s2.charAt(i) - 'a']++;
+        }
+        for(int i = 0; i < 26; i++)
+            if( s1Array[i] != s2Array[i] )
+                return false;   
+        
         boolean flag = false;
         for( int i=1;i<n;i++){
             
