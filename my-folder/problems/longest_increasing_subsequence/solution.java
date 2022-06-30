c1c15a691bed55b016356eceb076d0c7c9194bb1
Nitish Sharma <nitss007@gmail.com>
2022-06-30 19:23:41 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- my-folder/problems/longest_increasing_subsequence/solution.java
+++ my-folder/problems/longest_increasing_subsequence/solution.java
@@ -1,23 +1,31 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-        
-        int[] tails = new int[nums.length];
-	    int size = 0;
-	    for(int num:nums) {
-	    	int i=0, j= size;
-	    	while(i<j) {
-	    		int mid = (i+j)/2;
-	    		if(num>tails[mid]) {
-	    			i = mid+1;
-	    		}else {
-	    			j= mid;
-	    		}
-	    	}
-	    	if(i==size) size++;
-	    	tails[i] = num;
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp,1);
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<i;j++){
+                if(nums[i] > nums[j])
+                    dp[i] = Math.max(dp[i], dp[j]+1);
+            }
+        }
+        return IntStream.of(dp).max().orElse(0);
+//         int[] tails = new int[nums.length];
+// 	    int size = 0;
+// 	    for(int num:nums) {
+// 	    	int i=0, j= size;
+// 	    	while(i<j) {
+// 	    		int mid = (i+j)/2;
+// 	    		if(num>tails[mid]) {
+// 	    			i = mid+1;
+// 	    		}else {
+// 	    			j= mid;
+// 	    		}
+// 	    	}
+// 	    	if(i==size) size++;
+// 	    	tails[i] = num;
 	    
-	    }
-	    return size;
+// 	    }
+// 	    return size;
         
         // TreeSet<Integer> tset = new TreeSet<>();
         // for(int i=0;i<nums.length;i++){
