a8d856b46fda3850277c3090b998831a1dbb6831
Nitish Sharma <nitss007@gmail.com>
2022-07-04 06:31:13 +0000

    Sync LeetCode submission - Wiggle Subsequence (java)

--- my-folder/problems/wiggle_subsequence/solution.java
+++ my-folder/problems/wiggle_subsequence/solution.java
@@ -1,45 +1,16 @@
 class Solution {
-    
- public int wiggleMaxLength(int[] nums) {
-        if( nums.length == 0 ) return 0;
-        
-        int[] up = new int[nums.length];
-        int[] down = new int[nums.length];
-        
-        up[0] = 1;
-        down[0] = 1;
-        
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]>nums[i-1]){
-                up[i] = down[i-1]+1;
-                down[i] = down[i-1];
-            }else if(nums[i]<nums[i-1]){
-                down[i] = up[i-1]+1;
-                up[i] = up[i-1];
-            }else{
-                up[i] = up[i-1];
-                down[i] = down[i-1];
+        public int wiggleMaxLength(int[] nums) {
+        if(nums.length == 0) return 0;
+        int count = 1;
+        int prevDiff = 0;
+        for (int i = 1; i < nums.length; i++) {
+            int diff = nums[i] - nums[i-1];
+            if((diff <0 && prevDiff>=0) || (diff >0 && prevDiff<=0)){
+                count++;
+                prevDiff = diff;
             }
         }
-        
-        return Math.max(down[nums.length-1],up[nums.length-1]);
+        return count;
     }
-    
-//     public int wiggleMaxLength(int[] nums) {
-//         if(nums.length<1) return 1;
-//         return wiggleMaxLength(nums,1);
-//     }
-//     public int wiggleMaxLength(int[] nums, int n) {
-//         if(n>=nums.length)
-//             return 0;
-//         if(nums[n]>nums[n-1]){
-//             if(n+1<nums.length && nums[n]>nums[n+1]){
-//                 return Math.max(3,2+wiggleMaxLength(nums,n+2));
-//             }
-//             if(n+1>=nums.length)
-//                 return 2;
-            
-//         }
-//         return -1;    
-//     }    
 }
+// 1 16 -12 5 3 2 -5 11 -8
