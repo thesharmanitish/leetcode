2d651a3a0afc85fa5b29c649f1c28332e751f2c3
Nitish Sharma <nitss007@gmail.com>
2022-07-21 18:21:06 +0000

    Sync LeetCode submission - Intersection of Two Linked Lists (java)

--- my-folder/problems/intersection_of_two_linked_lists/solution.java
+++ my-folder/problems/intersection_of_two_linked_lists/solution.java
@@ -11,42 +11,52 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        ListNode rootA = headA, rootB = headB;
-        while(headA != null && headB!=null){
-            headA = headA.next;
-            headB = headB.next;
-        }
-        int countA =0, countB = 0;
-        if(headA !=null){
-            while(headA !=null){
-                if(headB == headA)
-                    return headB;
-                headA= headA.next;
-                countA++;
-            }
-        }else if(headB != null){
-            while(headB!=null){
-                if(headB == headA)
-                return headB;
-            headB = headB.next;
-            countB++;
-            }
-        }
-        headA = rootA;
-        headB = rootB;
-        while(countA-- >0){
-            headA = headA.next;
-        }
-        while(countB-- >0){
-            headB = headB.next;
-        }
         
         
-        while(headA!=null && headB!=null && headA != headB){
-            headA = headA.next;
-            headB = headB.next;
-        }        
-        
-        return headA;
+        ListNode a = headA, b = headB;
+        while(a != b){
+            a = a == null ? headB:a.next;
+            b = b == null ? headA:b.next;
+            
+        }
+        return a;
     }
+//         ListNode rootA = headA, rootB = headB;
+//         while(headA != null && headB!=null){
+//             headA = headA.next;
+//             headB = headB.next;
+//         }
+//         int countA =0, countB = 0;
+//         if(headA !=null){
+//             while(headA !=null){
+//                 if(headB == headA)
+//                     return headB;
+//                 headA= headA.next;
+//                 countA++;
+//             }
+//         }else if(headB != null){
+//             while(headB!=null){
+//                 if(headB == headA)
+//                 return headB;
+//             headB = headB.next;
+//             countB++;
+//             }
+//         }
+//         headA = rootA;
+//         headB = rootB;
+//         while(countA-- >0){
+//             headA = headA.next;
+//         }
+//         while(countB-- >0){
+//             headB = headB.next;
+//         }
+        
+        
+//         while(headA!=null && headB!=null && headA != headB){
+//             headA = headA.next;
+//             headB = headB.next;
+//         }        
+        
+//         return headA;
+//     }
 }
