770fd56a03e94198ec731708333dc98fb8f5cc4d
Nitish Sharma <nitss007@gmail.com>
2022-07-22 05:40:38 +0000

    Sync LeetCode submission - Partition List (java)

--- my-folder/problems/partition_list/solution.java
+++ my-folder/problems/partition_list/solution.java
@@ -10,31 +10,40 @@
  */
 class Solution {
     public ListNode partition(ListNode head, int x) {
-        if(head ==null) 
+        if(head == null)
             return head;
-        ListNode rightroot = new ListNode(-1), prev = null, right = rightroot, root = head;
-        ListNode node = root;
-        while(root!=null){
-            if(root.val>= x){
-                right.next = root;
-                right = right.next;
-                root = root.next;
-                right.next = null;
-                
-            }else{
-                if(prev !=null)
-                    prev.next = root;
-                else 
-                    node =root;
-                prev = root;
-                root = root.next;
-            }
-            
-        }
-        rightroot = rightroot.next;
-        if(prev!=null)
-            prev.next = rightroot;
         
-       return node;
+        ListNode right = new ListNode(-1);
+        
+        ListNode prev = null, rightHead = right, root = head;
+        while(head != null){
+            if(head.val <x){
+                prev = head;
+                head = head.next;
+                
+            }
+            else{
+                if(prev ==null){
+                    root = head.next;
+                    right.next = head;
+                    right = right.next;
+                    head = head.next;
+                    right.next = null;
+                    
+                }else{
+                    prev.next = head.next;
+                    
+                    right.next = head;
+                    right = right.next;
+                    head = head.next;
+                    right.next = null;
+                }
+            }
+        }
+        if(prev!=null)
+            prev.next = rightHead.next;
+        else
+            root = rightHead.next;
+        return root;
     }
 }
