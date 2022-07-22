34171d71911b37528385c12477b228c9aa7fc02a
Nitish Sharma <nitss007@gmail.com>
2022-07-22 17:12:04 +0000

    Sync LeetCode submission - Remove Duplicates from Sorted Array (java)

---
+++ my-folder/problems/remove_duplicates_from_sorted_array/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int prev = nums[0], start = 1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]>prev){
+                nums[start++] = nums[i]; 
+                prev = nums[i];
+            }
+                
+        }
+        return start;
+    }
+}
