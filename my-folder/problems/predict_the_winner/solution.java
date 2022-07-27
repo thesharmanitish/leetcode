afb6afadcd50a724337a79d41692f20c5ae3a176
Nitish Sharma <nitss007@gmail.com>
2022-07-27 17:32:25 +0000

    Sync LeetCode submission - Predict the Winner (java)

--- my-folder/problems/predict_the_winner/solution.java
+++ my-folder/problems/predict_the_winner/solution.java
@@ -1,14 +1,19 @@
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
-        return PredictTheWin(nums, 0, nums.length-1, 1) >=0;
-
+        dp = new Integer[nums.length+1][nums.length+1];
+        return getMaxScore(nums,0,nums.length-1) >= 0;
     }
-    public int PredictTheWin(int[] nums, int start, int end, int turn) {
-        if(start == end) 
-            return turn * nums[start];
-        int a = turn*nums[start] + PredictTheWin(nums, start+1,end,(-1)*turn);
-        int b = turn*nums[end] + PredictTheWin(nums, start,end-1,(-1)*turn);
-        return turn * Math.max(turn*a,turn*b);
+    Integer[][] dp;
+    private int getMaxScore(int[] nums, int s, int e){
+        if (s == e)
+            return dp[s][e] = nums[s];
+        if(dp[s][e] !=null)
+            return dp[s][e];
+        dp[s+1][e] = getMaxScore(nums,s+1,e);
+        int first = nums[s] - dp[s+1][e];
+        dp[s][e-1] = getMaxScore(nums,s,e-1);
+        int last = nums[e] - dp[s][e-1];
         
+        return dp[s][e] = Math.max(first,last);
     }
 }
