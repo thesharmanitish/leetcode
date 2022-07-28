657faaaf98d967fc117b7f3f4a66d5a2c09ba00b
Nitish Sharma <nitss007@gmail.com>
2022-07-28 07:47:32 +0000

    Sync LeetCode submission - Swap Salary (mysql)

--- my-folder/problems/swap_salary/solution.sql
+++ my-folder/problems/swap_salary/solution.sql
@@ -1,10 +1,11 @@
-/* Write your PL/SQL query statement below */
--- UPDATE salary
--- SET sex = CASE WHEN sex = "m" 
---                 THEN "f" 
---                 ELSE "m"
---            END
+# Write your MySQL query statement below
+# update Salary
+# set sex = if (sex = 'f','m', 'f')
 
-update salary set sex = case when sex = 'm' then 'f'
-                             else 'm'
-                             end
+ UPDATE salary
+ SET sex = CASE WHEN sex = "m" 
+                 THEN "f" 
+                ELSE "m"
+           END
+                             
+# UPDATE salar  y SET sex = REPLACE ('fm', sex, '');
