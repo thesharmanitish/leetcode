9f6515e1225d1b1495ec40e5850bcd340107ed9e
Nitish Sharma <nitss007@gmail.com>
2022-08-09 16:19:39 +0000

    Sync LeetCode submission - Sort an Array (java)

--- my-folder/problems/sort_an_array/solution.java
+++ my-folder/problems/sort_an_array/solution.java
@@ -5,30 +5,33 @@ class Solution {
 	         return nums;   
 	    }
 	    public static  void quickSort(int[] nums, int start, int end) {
-             int pivot = partition(nums, start, end);
-             if(start< pivot-1)
-	            quickSort(nums, start, pivot-1);
-             if(pivot<end)
-                quickSort(nums,pivot, end);
+            int pivot = partition(nums, start , end);
+            
+            if(start< pivot-1)
+                quickSort(nums, start, pivot-1);
+            if(end> pivot)
+                quickSort(nums, pivot, end);
 	            
 	    }	    
 	 	public static  int partition(int[] nums, int start, int end) {
-            int i = start, j = end;
-            int pivot = nums[(start + end) / 2];
-            int temp;
-            while(i<=j){
-                while(nums[i] < pivot)
-                    i++;
-                while(nums[j] > pivot)
-                    j--;
-                if (i <= j) {
-                    temp = nums[i];
-                    nums[i]  = nums[j];
-                    nums[j] = temp;
-                    i++;
-                    j--;       
+            int pivot = nums[(end+start)/2];
+            
+            while(start<=end){
+                while(nums[start]<pivot)
+                    start++;
+                while(nums[end]>pivot)
+                    end--;
+                if(start<=end){
+                    swap(nums, start++, end--);
+                    
                 }
             }
-            return i;           
+            return start;
         }
+    
+	    public static  void swap(int[] nums, int i, int j) {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+	    }    
 }
