0b53738f08b4006053963f0cadb1a0f4feeab06b
Nitish Sharma <nitss007@gmail.com>
2022-08-15 03:40:42 +0000

    Sync LeetCode submission - Roman to Integer (java)

---
+++ my-folder/problems/roman_to_integer/solution.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int romanToInt(String s) {
+        int result = 0;
+        for(int i=0 ;i<s.length();i++){
+            switch(s.charAt(i)){
+                case 'M': result += 1000; break;
+                case 'D': result += 500;break;
+                case 'C': if(i+1>= s.length())
+                                result+=100;
+                            else if(s.charAt(i+1) == 'D'){
+                                result +=400;
+                                i++;
+                            }
+                           else if(s.charAt(i+1) == 'M'){
+                                 result +=900;
+                                 i++;
+                            }
+                          else result+=100;
+                          break;  
+                case 'L': result+=50;
+                          break;
+                case 'X':  
+                            if(i+1>= s.length())
+                                result+=10;
+                            else if(s.charAt(i+1) == 'L'){
+                                result +=40;
+                                i++;
+                            }
+                           else if(s.charAt(i+1) == 'C'){
+                                 result +=90;
+                                 i++;
+                            }
+                          else result+=10;
+                          break;
+                case 'V': result += 5; break;    
+                case 'I': 
+                            if(i+1>=s.length())
+                                result+=1;
+                            else if(s.charAt(i+1) == 'V'){
+                                result +=4;
+                                i++;
+                            }
+                           else if(s.charAt(i+1) == 'X'){
+                                 result +=9;
+                                 i++;
+                            }else
+                               result +=1;
+                          
+                          break;    
+            }
+        }
+        return result;
+            
+    }
+}
