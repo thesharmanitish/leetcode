8bb107f780427d92962202f354b92bb48f58a7d6
Nitish Sharma <nitss007@gmail.com>
2022-08-20 14:00:09 +0000

    Sync LeetCode submission - Minimum Number of Refueling Stops (java)

---
+++ my-folder/problems/minimum_number_of_refueling_stops/solution.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int minRefuelStops(int target, int startFuel, int[][] stations) {
+        int[][] dp = new int[stations.length+1][stations.length+1];
+        
+        for(int i=0;i<=stations.length;i++){
+            dp[i][0] = startFuel;
+        }
+        
+        for(int i=1;i<=stations.length;i++){
+            for(int j=1;j<=i;j++){
+                if(dp[i-1][j]>= stations[i-1][0])
+                    dp[i][j] =  dp[i-1][j];
+                if(dp[i-1][j-1]>= stations[i-1][0])
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1]+stations[i-1][1]);
+            }
+        }
+        for (int t = 0; t <= stations.length; ++t)
+            if (dp[stations.length][t] >= target) return t;
+
+        return -1;
+
+    }
+}
