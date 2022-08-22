07709859bac6ff9b552af8c72971aa0fe76dab5c
Nitish Sharma <nitss007@gmail.com>
2022-08-22 04:39:08 +0000

    Sync LeetCode submission - Power of Four (java)

--- my-folder/problems/power_of_four/solution.java
+++ my-folder/problems/power_of_four/solution.java
@@ -7,14 +7,11 @@ class Solution {
         int i=0;
         if((n & ( n-1))==0){
             return (n & 0x55555555) != 0;  // little hack as 0x555555 = 1010101010101
-// Working but less efficient
-//             while(n>0){
-//                 i++;
-//                 n = n>>1;
-                
-//             }
-//             if((i&1) ==1)
-//                 return true;
+            // return (n & 0xaaaaaaaa) ==0 ;
+            
+            //a =1010 & 100
+            // 5 = 10101 & 100
+
             
         }
         return false;
