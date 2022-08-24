62a9a2e08ba127143935a651f0005f3a45f7a3d2
Nitish Sharma <nitss007@gmail.com>
2022-08-24 17:32:45 +0000

    Sync LeetCode submission - Integer to Roman (java)

--- my-folder/problems/integer_to_roman/solution.java
+++ my-folder/problems/integer_to_roman/solution.java
@@ -1,21 +1,21 @@
-/*
-Symbol       Value
-I             1
-V             5
-X             10
-L             50
-C             100
-D             500
-M             1000
-*/
-
 class Solution {
-    private static final String[] thousands = {"", "M", "MM", "MMM"};
-    private static final String[] hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"}; 
-    private static final String[] tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    private static final String[] ones = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
-    
-    public String intToRoman(int num) { 
-        return thousands[num / 1000] + hundreds[num % 1000 / 100] + tens[num % 100 / 10] + ones[num % 10];
-    }
+    public static String intToRoman(int num){
+        if (num < 1 || num > 3999) return "";
+
+        StringBuilder result = new StringBuilder();
+
+        String[] roman = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+        int[] values = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 };
+
+        int i = 0;
+                //iterate until the number becomes zero, NO NEED to go until the last element in roman array
+        while (num >0) {
+            while ( num >= values[i]) {
+                num -= values[i];
+                result.append(roman[i]);
+            }
+            i++;
+        }
+        return result.toString();
+}
 }
