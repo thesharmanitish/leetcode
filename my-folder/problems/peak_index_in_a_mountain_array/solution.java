6beb1293da99d614f80011b4305ec151591cf480
Nitish Sharma <nitss007@gmail.com>
2022-08-26 16:13:17 +0000

    Sync LeetCode submission - Peak Index in a Mountain Array (java)

--- my-folder/problems/peak_index_in_a_mountain_array/solution.java
+++ my-folder/problems/peak_index_in_a_mountain_array/solution.java
@@ -1,19 +1,16 @@
 class Solution {
     public int peakIndexInMountainArray(int[] x) {
-        int mid =-1, start = 0, n = x.length, end = n-1;
+        int start = 0, n = x.length, end = n-1;
         
         while(start<=end){
-            mid = start+(end-start)/2;
-            
-            if(mid-1 >=0 && x[mid-1] > x[mid] ) {
-                end = mid -1;
-                
-            }else if(mid+1 < n && x[mid+1] > x[mid]){
-                start = mid+1;
-            }else
+            int mid = start + (end-start)/2;
+            if(mid>0 && x[mid-1]<x[mid] && mid<n && x[mid+1]<x[mid])
                 return mid;
-            
+            else if(mid>0 && x[mid-1]>x[mid])
+                end = mid-1;
+            else if(mid<n && x[mid+1]>x[mid])
+                start = mid +1;
         }
-        return mid;
+        return start;
     }
 }
