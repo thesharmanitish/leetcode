4fe8da7e3ac0e5b96b4272d88e411adc998b5434
Nitish Sharma <nitss007@gmail.com>
2022-08-27 19:49:44 +0000

    Sync LeetCode submission - Maximum Subarray (java)

--- my-folder/problems/maximum_subarray/solution.java
+++ my-folder/problems/maximum_subarray/solution.java
@@ -1,30 +1,44 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        return findBestSubarray(nums, 0, nums.length - 1);
-    }
-    
-    private int findBestSubarray(int[] nums, int left, int right) {
-        // Base case - empty array.
-        if (left > right) {
-            return Integer.MIN_VALUE;
-        }
-        int mid = (left + right) >>1;
-        int cur =0, maxLeft=0, maxRight=0, max= 0;
+        if(nums.length ==0)
+            return -1;
+        int curMax = 0, max = nums[0];
         
-        for(int i=mid-1;i>=left;i--){
-            cur += nums[i];
-            maxLeft = Math.max(cur, maxLeft);
+        for(int i=0;i<nums.length;i++){
+            curMax = Math.max(nums[i], curMax +nums[i]);
+            max = Math.max(max, curMax)    ;
         }
-        cur =0;
-        for(int i=mid+1;i<=right;i++){
-            cur += nums[i];
-            maxRight = Math.max(cur, maxRight);            
-        }
-        max = nums[mid]+maxLeft+maxRight;
-        
-        int leftHalf = findBestSubarray(nums, left, mid - 1);
-        int rightHalf = findBestSubarray(nums, mid + 1, right);        
-        
-        return Math.max(max, Math.max(leftHalf, rightHalf));
+        return max;
     }
 }
+
+// class Solution {
+//     public int maxSubArray(int[] nums) {
+//         return findBestSubarray(nums, 0, nums.length - 1);
+//     }
+    
+//     private int findBestSubarray(int[] nums, int left, int right) {
+//         // Base case - empty array.
+//         if (left > right) {
+//             return Integer.MIN_VALUE;
+//         }
+//         int mid = (left + right) >>1;
+//         int cur =0, maxLeft=0, maxRight=0, max= 0;
+        
+//         for(int i=mid-1;i>=left;i--){
+//             cur += nums[i];
+//             maxLeft = Math.max(cur, maxLeft);
+//         }
+//         cur =0;
+//         for(int i=mid+1;i<=right;i++){
+//             cur += nums[i];
+//             maxRight = Math.max(cur, maxRight);            
+//         }
+//         max = nums[mid]+maxLeft+maxRight;
+        
+//         int leftHalf = findBestSubarray(nums, left, mid - 1);
+//         int rightHalf = findBestSubarray(nums, mid + 1, right);        
+        
+//         return Math.max(max, Math.max(leftHalf, rightHalf));
+//     }
+// }
