8b8e09e0c2bb40dfe171ba2daef13cc0b0454b4e
Nitish Sharma <nitss007@gmail.com>
2022-08-28 18:50:37 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock II (java)

--- my-folder/problems/best_time_to_buy_and_sell_stock_ii/solution.java
+++ my-folder/problems/best_time_to_buy_and_sell_stock_ii/solution.java
@@ -1,17 +1,31 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int max = 0, peak =0, valley = 0, i=0;
+//         int max = 0, peak =0, valley = 0, i=0;
         
-        while(i<prices.length-1){
-            while(i< prices.length-1 && prices[i] >= prices[i+1])
-                i++;
-            valley = prices[i];
-            while(i< prices.length-1 && prices[i] <= prices[i+1])
-                i++;           
-            peak = prices[i];
+//         while(i<prices.length-1){
+//             while(i< prices.length-1 && prices[i] >= prices[i+1])
+//                 i++;
+//             valley = prices[i];
+//             while(i< prices.length-1 && prices[i] <= prices[i+1])
+//                 i++;           
+//             peak = prices[i];
             
-            max +=peak -valley;
+//             max +=peak -valley;
+//         }
+//         return max;
+        
+        
+        
+        int sum = 0, prev = Integer.MAX_VALUE;
+        for(int price:prices){
+            if(prev>price ){
+                prev = price;
+                continue;
+            }
+            sum += price-prev;
+            prev = price;
+                
         }
-        return max;
+        return sum;
     }
 }
