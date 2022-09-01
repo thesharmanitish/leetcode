42d14423f39675c8417bbbcbea1d3c4cba55e23f
Nitish Sharma <nitss007@gmail.com>
2022-09-01 06:21:25 +0000

    Sync LeetCode submission - Number of Ways to Select Buildings (java)

--- my-folder/problems/number_of_ways_to_select_buildings/solution.java
+++ my-folder/problems/number_of_ways_to_select_buildings/solution.java
@@ -1,44 +1,27 @@
 class Solution {
-    
-    public static long numberOfWays(String s) {
-		int zeroCount = 0, oneCount = 0;
-		for(char c:s.toCharArray())
-			if(c=='0')
-				zeroCount++;
-		oneCount = s.length()-zeroCount;
-		
-		int firstOne = s.charAt(0)=='1'? 1:0;
-		int firstZero = s.charAt(0)=='0'? 1:0;
-		long count = 0;
-		for(int i=1;i<s.length();i++) {
-			if(s.charAt(i)=='0') {
-				count+=firstOne*(oneCount-firstOne);
-                firstZero++;
-			}else {
-				count+=firstZero*(zeroCount-firstZero);
-                firstOne++;
-			}
-		}
-		return count;
-		
-	}
-    
-    public long numberOfWaysAlternate(String s) {
-	
-    long  _0 = 0, _1 = 0, _01 = 0, _10 = 0, _010 = 0, _101 = 0;
-    for(char c : s.toCharArray()) 
-        if(c == '0') {
-            _010 += _01;     
-            _10  += _1;        
-            _0   += 1;
+    public long numberOfWays(String s) {
+        char[] arr = s.toCharArray();
+        
+        int oneCount = 0, zeroCount = 0;
+        for(char c:arr){
+            if(c=='1')
+                oneCount +=1;
         }
-        else {
-            _101 += _10;
-            _01  += _0;
-            _1   += 1;
+        zeroCount = arr.length - oneCount;
+        
+        
+        int curZero = arr[0] == '0'?1:0;
+        int curOne =  arr[0] == '1'?1:0;
+        long sum =0;
+        for(int i=1;i<arr.length;i++){
+            if(arr[i] =='0'){
+                sum = sum+ curOne*(oneCount-curOne);
+                curZero++;
+            }else{
+                sum = sum+ curZero*(zeroCount-curZero);
+                curOne++;
+            }
         }
-                
-    return _101 + _010;
-	
+        return sum;
     }
 }
