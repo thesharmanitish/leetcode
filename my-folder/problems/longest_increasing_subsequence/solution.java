085ffe53b4c2d4cd5673c4e8e2fb3126a5fbf7dd
Nitish Sharma <nitss007@gmail.com>
2022-09-02 03:02:56 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- my-folder/problems/longest_increasing_subsequence/solution.java
+++ my-folder/problems/longest_increasing_subsequence/solution.java
@@ -2,14 +2,17 @@ class Solution {
     public int lengthOfLIS(int[] nums) {
         int[] dp = new int[nums.length];
         Arrays.fill(dp,1);
-        
+        int max = dp[0];
         for(int i=1;i<nums.length;i++){
             for(int j=0;j<i;j++){
-                if(nums[i]>nums[j])
-                    dp[i] = Math.max(dp[i], dp[j]+1);
+                if(nums[i] >nums[j])
+                    dp[i] = Math.max(dp[i], 1+dp[j]);
             }
+            max = Math.max(dp[i], max);
         }
-        return IntStream.of(dp).max().orElse(0);
+        
+        return max;
+        
         
 //         int[] tails = new int[nums.length];
 // 	    int size = 0;
