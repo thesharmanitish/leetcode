d31daa1f4b02341c42176223685f890f71433ad8
Nitish Sharma <nitss007@gmail.com>
2022-09-06 02:25:54 +0000

    Sync LeetCode submission - Letter Combinations of a Phone Number (java)

--- my-folder/problems/letter_combinations_of_a_phone_number/solution.java
+++ my-folder/problems/letter_combinations_of_a_phone_number/solution.java
@@ -1,41 +1,97 @@
 class Solution {
-    char[] two={'a','b','c'}, three={'d','e','f'}, four={'g','h','i'}, five = {'j','k','l'}, six = {'m','n','o'}
-           ,seven = {'p','q','r','s'}, eight={'t','u','v'}, nine={'w','x','y','z'}, zero = {' '};
-    
-    public List<String> letterCombinations(String digits) {
-        char[] in = digits.toCharArray();
-        List<String> res = new ArrayList<>();
-        for(char c:digits.toCharArray()){
-            switch(c){
-                    case '2' :  res = letterCombinations(two, res);break;
-                    case '3' :  res = letterCombinations(three, res);break;
-                    case '4' :  res = letterCombinations(four, res);break;
-                    case '5' :  res = letterCombinations(five, res);break;
-                    case '6' :  res = letterCombinations(six, res);break;
-                    case '7' :  res = letterCombinations(seven, res);break;
-                    case '8' :  res = letterCombinations(eight, res);break;
-                    case '9' :  res = letterCombinations(nine, res);break;
-                    case '0' :  res = letterCombinations(zero, res);break;
-            }
+
+    static Map<Character, String> NUMS = new HashMap() {
+        {
+            put('0', "");
+            put('1', "");
+            put('2', "abc");
+            put('3', "def");
+            put('4', "ghi");
+            put('5', "jkl");
+            put('6', "mno");
+            put('7', "pqrs");
+            put('8', "tuv");
+            put('9', "wxyz");
         }
+    };
+
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) {
+            return new ArrayList<String>();
+        }
+        StringBuilder sb = new StringBuilder();
+        List<String> res = new ArrayList<>();
+        dfs(sb, 0, digits, NUMS, res);
+
         return res;
     }
-    public List<String>  letterCombinations(char[] digits, List<String> res) {
-       List<String> tmp = new ArrayList<>();
+
+    private void dfs(StringBuilder sb, int index, String digits, Map<Character, String> nums, List<String> res) {
+        if(sb.length() == digits.length()){
+            res.add(sb.toString());
+            return;
+        }
         
-        if(res.size() ==0){
-            for(char c:digits){
-                tmp.add(""+c);
-            }               
+        for(char c: nums.get(digits.charAt(index)).toCharArray()){
+            sb.append(c);
+            
+            dfs(sb, index+1, digits, nums, res);
+            
+            sb.deleteCharAt(sb.length()-1);
         }
-        else{
-            for(String s:res){
-                for(char c:digits){
-                    tmp.add(s+c);
-                }
-            }
-        }
-        return tmp;
         
     }
 }
+
+// class Solution {
+//     char[] 
+//         two={'a','b','c'}, 
+//         three={'d','e','f'}, 
+//         four={'g','h','i'},     
+//         five = {'j','k','l'}, 
+//         six = {'m','n','o'},
+//         seven = {'p','q','r','s'}, 
+//         eight={'t','u','v'}, 
+//         nine={'w','x','y','z'}, 
+//         zero = {' '};
+    
+//     public List<String> letterCombinations(String digits) {
+//         List<String> result = new ArrayList<>();
+//         for(char c:digits.toCharArray()){
+//             switch(c){
+//                 case '2': result = letterCombinations(two, result);break;
+//                 case '3': result = letterCombinations(three, result);break; 
+//                 case '4': result = letterCombinations(four, result);break;
+//                 case '5': result = letterCombinations(five, result);break;
+//                 case '6': result = letterCombinations(six, result);break;
+//                 case '7': result = letterCombinations(seven, result);break;
+//                 case '8': result = letterCombinations(eight, result);break;
+//                 case '9': result = letterCombinations(nine, result);break;
+//                 case '0': result = letterCombinations(zero, result);break;    
+//             }
+            
+//         }
+        
+//         return result;
+//     }
+//     public List<String>  letterCombinations(char[] digits, List<String> res) {
+//         List<String> result = new ArrayList<>();
+//         if(res.size()==0){
+//             for(char n:digits){
+//                 result.add(""+n);
+                
+//             }
+//             return result;
+//         }
+//         for(char n:digits){
+//             for(String s:res){
+//                 result.add(s+n);
+//             }
+            
+//         }
+        
+        
+//         return result;
+        
+//     }
+// }
