0e50a5940b9ebca3d967f9761361bf418e91fc1b
Nitish Sharma <nitss007@gmail.com>
2022-09-08 16:37:41 +0000

    Sync LeetCode submission - N-ary Tree Preorder Traversal (java)

---
+++ my-folder/problems/n-ary_tree_preorder_traversal/solution.java
@@ -0,0 +1,34 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+    List<Integer> result = new ArrayList<>();
+    public List<Integer> preorder(Node root) {
+        inorderTraversalUtil(root);
+        return result;
+    }
+    public void inorderTraversalUtil(Node root) {
+        if(root  == null)
+            return;
+        result.add(root.val);
+        for(Node node:root.children)
+            inorderTraversalUtil(node);
+        
+    }
+}
