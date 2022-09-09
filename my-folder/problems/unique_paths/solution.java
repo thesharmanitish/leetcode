ad75d88f7c118026e5bc059a2c194cda9783caa0
Nitish Sharma <nitss007@gmail.com>
2022-09-09 19:06:16 +0000

    Sync LeetCode submission - Unique Paths (java)

--- my-folder/problems/unique_paths/solution.java
+++ my-folder/problems/unique_paths/solution.java
@@ -3,6 +3,7 @@ class Solution {
      
         int[][] dp = new int[m][n];
         dp[0][0] = 1;
+        
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(j<1 && i<1)
@@ -11,9 +12,8 @@ class Solution {
                     dp[i][j] = dp[i-1][j];
                 else if(i<1)
                     dp[i][j] = dp[i][j-1];
-                else
+                else 
                     dp[i][j] = dp[i][j-1] + dp[i-1][j];
-                    
             }
         }
         return dp[m-1][n-1];
