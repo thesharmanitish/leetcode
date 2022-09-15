2061e6bb2f3f525deaa882423805ccf93c56938a
Nitish Sharma <nitss007@gmail.com>
2022-09-15 05:09:14 +0000

    Sync LeetCode submission - Course Schedule (java)

--- my-folder/problems/course_schedule/solution.java
+++ my-folder/problems/course_schedule/solution.java
@@ -1,5 +1,41 @@
 class Solution {
-    public boolean canFinish(int n, int[][] prerequisites) {
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        HashMap<Integer, List<Integer>> map = new HashMap<>();
+        HashMap<Integer, Boolean> seen = new HashMap<>();
+        int[] indegree = new int[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+        int count = 0;
+        
+        // Building the graph
+        for(int[] courses : prerequisites) {
+            int to = courses[0];
+            int from = courses[1];
+            var list = map.getOrDefault(from, new ArrayList<>());
+            list.add(to);
+            map.put(from, list);
+            indegree[to]++;
+        }
+        
+        // Implementation of Kahn's Alg
+        for(int i = 0; i < numCourses; i++) {
+            if(indegree[i] == 0) q.add(i);
+        }
+        while(!q.isEmpty()) {
+            int node = q.peek();
+            q.poll();
+            count++;
+            if(map.containsKey(node)) {
+                for(int nei : map.get(node)) {
+                    indegree[nei]--;
+                    if(indegree[nei] == 0) {
+                        q.add(nei);
+                    }
+                }
+            }
+        }
+        return count == numCourses;
+//     }
+//     public boolean canFinish(int numCourses, int[][] prerequisites) {
 //         if(prerequisites.length==0) return true;
 //         boolean[][] grid = new boolean[numCourses][numCourses];
 //         int[] indegree = new int[numCourses];
@@ -24,20 +60,21 @@ class Solution {
 //                 }
 //             }
 //         }
-//         return count==numCourses;
-               ArrayList<Integer>[] G = new ArrayList[n];
-        int[] degree = new int[n];
-        ArrayList<Integer> bfs = new ArrayList();
-        for (int i = 0; i < n; ++i) G[i] = new ArrayList<Integer>();
-        for (int[] e : prerequisites) {
-            G[e[1]].add(e[0]);
-            degree[e[0]]++;
-        }
-        for (int i = 0; i < n; ++i) if (degree[i] == 0) bfs.add(i);
-        for (int i = 0; i < bfs.size(); ++i)
-            for (int j: G[bfs.get(i)])
-                if (--degree[j] == 0) bfs.add(j);
-        return bfs.size() == n;
+//        return count==numCourses;
+        // ArrayList<Integer>[] G = new ArrayList[n];
+        // int[] degree = new int[n];
+        // ArrayList<Integer> bfs = new ArrayList();
+        // for (int i = 0; i < n; ++i) 
+        //     G[i] = new ArrayList<Integer>();
+        // for (int[] e : prerequisites) {
+        //     G[e[1]].add(e[0]);
+        //     degree[e[0]]++; 
+        // }
+        // for (int i = 0; i < n; ++i) if (degree[i] == 0) bfs.add(i);
+        // for (int i = 0; i < bfs.size(); ++i)
+        //     for (int j: G[bfs.get(i)])
+        //         if (--degree[j] == 0) bfs.add(j);
+        // return bfs.size() == n;
     }
 
 }
