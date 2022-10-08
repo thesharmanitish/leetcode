7ea5d9f70e706d6a5ea698483afb23ca7e889ece
Nitish Sharma <nitss007@gmail.com>
2022-10-08 02:33:45 +0000

    Sync LeetCode submission - 3Sum Closest (java)

--- my-folder/problems/3sum_closest/solution.java
+++ my-folder/problems/3sum_closest/solution.java
@@ -1,33 +1,24 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
-        
-        int max = 0;
-        if(nums.length<3) return max;
         Arrays.sort(nums);
-        boolean flag = false;
-        for(int i=0;i<nums.length-2;i++){
-            int j=i+1,k=nums.length-1;
-            
+        int sum = 0, diff = Integer.MAX_VALUE, min = 0;
+        for(int  i=0;i<nums.length;i++){
+        int j=i+1, k=nums.length-1;    
             while(j<k){
+                sum = nums[i]+nums[j]+nums[k];
+                int t = target-sum;
                 
-                    
-                int sum = nums[i]+nums[j]+nums[k];
-                if(!flag) {
-                    flag =true;
-                    max = sum;
-                }
-                if(sum<target) j++;
-                else if(sum>target)k--;
-                else return sum;
-                    
-                if(Math.abs(max-target)>Math.abs(sum-target))
-                    max = sum;
-                
+                if(Math.abs(t) < Math.abs(diff))
+                    diff = t;
+                if(sum < target){
+                    j++;
+                }else{
+                    k--;
+                }               
             }
-        
-        
         }
-        return max;
-        
+        return target - diff;
     }
 }
+
+// -5 -5 -4 0 0 3 3 4 5-
