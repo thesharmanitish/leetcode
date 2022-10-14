49e4ce6dfd0a42cfd11009b3e91dffbbf8280ee2
Nitish Sharma <nitss007@gmail.com>
2022-10-14 11:19:39 +0000

    Sync LeetCode submission - Number of Islands (java)

--- my-folder/problems/number_of_islands/solution.java
+++ my-folder/problems/number_of_islands/solution.java
@@ -1,25 +1,29 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        // boolean[][] visited  = new boolean[grid.length][grid[0].length];
+        if (grid == null || grid.length == 0) {
+          return 0;
+        }        
         int count = 0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j]=='0')
+                if(grid[i][j] == '0')
                     continue;
-				count++;
-                validNeighbour(grid,i,j);                
-                
+                dfs(grid, i, j);
+                count+=1;
             }
         }
         return count;
     }
-    public void validNeighbour(char[][] grid, int i, int j) {
-        if(i>=grid.length || j>= grid[0].length || i<0 || j<0 || grid[i][j]=='0' )
-            return ;
-        
+    public void dfs(char[][] grid, int i, int j) {
+        if(i<0 || j<0 || i>=grid.length || j>= grid[0].length|| grid[i][j] =='0')
+            return;
         grid[i][j] = '0';
-        validNeighbour(grid,i-1,j);
-        validNeighbour(grid,i+1,j);
-        validNeighbour(grid,i,j-1);
-        validNeighbour(grid,i,j+1);
+        
+        dfs(grid, i+1, j );
+        dfs(grid, i-1, j );
+        dfs(grid, i, j+1 );
+        dfs(grid, i, j-1 );
+            
     }
 }
