0940b4ebbc801f0f3eec71735f71401176bbecea
Nitish Sharma <nitss007@gmail.com>
2022-10-14 16:59:22 +0000

    Sync LeetCode submission - Is Subsequence (java)

--- my-folder/problems/is_subsequence/solution.java
+++ my-folder/problems/is_subsequence/solution.java
@@ -1,20 +1,29 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
+        // int i=0;
+        // if(s.length() ==0)
+        //     return true;
+        // for(char c:t.toCharArray()){
+        //     if(c == s.charAt(i))
+        //         i++;
+        //     if(i == s.length())
+        //         return true;
+        // }
+        // return i == s.length();
+            
         int[][] dp = new int[s.length()+1][t.length()+1];
         int j=0;
         if(s.length() ==0)
             return true;
-        for(j=1;j<=t.length();j++){
-            for(int i=1;i<=s.length();i++){
+        for(int i=1;i<=s.length();i++){
+            for(j=1;j<=t.length();j++){
                 if(s.charAt(i-1) == t.charAt(j-1))
-                    dp[i][j] = 1+dp[i-1][j-1];
-                else{
+                    dp[i][j] = 1+ dp[i-1][j-1];
+                else
                     dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
-                }
             }
-            if(s.length() == dp[s.length()][j])
-                return true;
+
         }
-        return false;
+        return dp[s.length()][t.length()] == s.length();
     }
 }
