f7841c6d4c2c8a9fd29ed925cfd8a72a8731e7bd
Nitish Sharma <nitss007@gmail.com>
2022-10-17 07:12:20 +0000

    Sync LeetCode submission - Minimum Difficulty of a Job Schedule (java)

--- my-folder/problems/minimum_difficulty_of_a_job_schedule/solution.java
+++ my-folder/problems/minimum_difficulty_of_a_job_schedule/solution.java
@@ -1,38 +1,33 @@
 class Solution {
+    static int[][] dp;
+    int n;
     public int minDifficulty(int[] jobDifficulty, int d) {
-
-        int n = jobDifficulty.length;
-        // Edge case: make sure there is at least one job per day
-        if (n < d) {
+        n = jobDifficulty.length;
+        if(d>n)
             return -1;
-        }
-
-        int[][] mem = new int[n][d + 1];
-        for (int i = 0; i < n; i++) {
-            Arrays.fill(mem[i],-1);
-        }
-
-        return minDiff(0, d, jobDifficulty, mem);
+        dp = new int[jobDifficulty.length+1][d+1];
+        
+        for(int i=0;i<dp.length;i++)
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        return solve(jobDifficulty, 0,d);
     }
+    public int solve(int[] jobDifficulty, int i, int d) {
 
-    private int minDiff(int i, int daysRemaining, int[] jobDifficulty, int[][] memo) {
-        if(memo[i][daysRemaining] !=-1)
-            return memo[i][daysRemaining];
-        
-        if(daysRemaining ==1){
-            int max = Integer.MIN_VALUE;
-            for(int start =i;start<jobDifficulty.length;start++)
-                max = Math.max(max, jobDifficulty[start]);
-            return max;
+        if(dp[i][d]!=Integer.MAX_VALUE)
+            return dp[i][d];
+        int max = 0;
+        if(d ==1){
+            
+            for(int j=i;j<n;j++)
+                max = Math.max(max, jobDifficulty[j]);
+            return dp[i][d]= max;
         }
-        
-        int maxDiff = 0;
-        int res = Integer.MAX_VALUE;
-        for(int start=i; start< jobDifficulty.length-daysRemaining+1;start++){
-            maxDiff = Math.max(maxDiff, jobDifficulty[start]);
-            res = Math.min(res , maxDiff +  minDiff(start+1, daysRemaining-1, jobDifficulty, memo));
+        int res =Integer.MAX_VALUE;
+        for(int j=i;j<n-d+1;j++){
+            max = Math.max(max, jobDifficulty[j]);
+            dp[i][d] = Math.min(dp[i][d], max+solve(jobDifficulty, j+1, d-1));
+            
         }
-        return memo[i][daysRemaining] =res;
-        
+        return dp[i][d];
     }
 }
