dda159521d5e7568183ea914bfe9b6286271cf1d
Nitish Sharma <nitss007@gmail.com>
2022-10-17 10:37:39 +0000

    Sync LeetCode submission - Group Anagrams (java)

--- my-folder/problems/group_anagrams/solution.java
+++ my-folder/problems/group_anagrams/solution.java
@@ -1,25 +1,32 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String,List<String>> map = new HashMap<>();
-        
-        for(String s:strs){
-            char[] arr = s.toCharArray();
-            Arrays.sort(arr);
-            String t = String.valueOf(arr);
-            if(map.get(t)!=null){
-                List<String> x = map.getOrDefault(t, new ArrayList<>());
-                x.add(s);
-                map.put(t,x);
-            }else{
-                List<String> x = new ArrayList<>();
-                x.add(s);
-                map.put(t, x);
+        Map<Long, List<String>> map = new HashMap<>();
+        for(String str:strs){
+            long sum=0;
+            for(char c:str.toCharArray()){
+                sum+=(long) c*(c-1)*(c*c+1)*(7919);
             }
+            map.computeIfAbsent(sum, (s)->new ArrayList<>()).add(str);
         }
-        List<List<String>> res = new ArrayList<>();
-        for(List<String> ll:map.values()){
-            res.add(ll);
-        }
-        return res;
+        
+        return map.values().stream().collect(Collectors.toList());
+        
+//         Map<String,List<String>> map = new HashMap<>();
+        
+//         for(String s:strs){
+//             char[] arr = s.toCharArray();
+//             Arrays.sort(arr);
+//             String t = String.valueOf(arr);
+//             if(map.get(t)!=null){
+//                 List<String> x = map.getOrDefault(t, new ArrayList<>());
+//                 x.add(s);
+//                 map.put(t,x);
+//             }else{
+//                 List<String> x = new ArrayList<>();
+//                 x.add(s);
+//                 map.put(t, x);
+//             }
+//         }
+//         return map.values().stream().collect(Collectors.toList());        
     }
 }
