249b3b10b2f67cefffad2fbcb9d2fd722aad1252
Nitish Sharma <nitss007@gmail.com>
2022-10-19 19:24:11 +0000

    Sync LeetCode submission - Top K Frequent Words (java)

--- my-folder/problems/top_k_frequent_words/solution.java
+++ my-folder/problems/top_k_frequent_words/solution.java
@@ -1,21 +1,17 @@
-
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>((a,b)-> {
-            return a.getValue() == b.getValue() ? a.getKey().compareTo(b.getKey())
-                :b.getValue().compareTo(a.getValue());});
-        Map<String, Integer> map = new HashMap<>();
+        PriorityQueue<Map.Entry<String,Integer>> pq = new PriorityQueue<>((a,b)-> { return b.getValue() == a.getValue()? a.getKey().compareTo(b.getKey()):b.getValue().compareTo(a.getValue());});
+        HashMap<String, Integer> map = new HashMap<>();
+        for(String word:words)
+            map.put(word, map.getOrDefault(word, 0)+1);
         
-        for(String word:words){
-            map.put(word, map.getOrDefault(word,0)+1);
-        }
-        for(Map.Entry<String, Integer> entry:map.entrySet())
+        for(Map.Entry<String,Integer> entry:map.entrySet())
             pq.add(entry);
-        List<String> result= new ArrayList<>();
+        List<String> result = new ArrayList<>();
+        if(k>pq.size())
+            return result;
         for(int i=0;i<k;i++)
             result.add(pq.poll().getKey());
-            
-        return result;   
-        
+        return result;
     }
 }
