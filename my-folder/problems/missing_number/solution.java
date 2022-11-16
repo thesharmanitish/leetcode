e1872a4fc46ce4c47cff5dc1816c09f2db40ff5e
Nitish Sharma <nitss007@gmail.com>
2022-11-16 18:11:48 +0000

    Sync LeetCode submission - Missing Number (java)

--- my-folder/problems/missing_number/solution.java
+++ my-folder/problems/missing_number/solution.java
@@ -1,10 +1,26 @@
 class Solution {
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] =  nums[j];
+        nums[j] = temp;
+    }
     public int missingNumber(int[] nums) {
-        int n = nums.length, t=0;
-        int sum = (n*(n+1)) >>1;
-        for(int x:nums)
-            t+=x;
-        return sum-t;
+        int i=0;
+        int n = nums.length;
+        while(i<n)
+        {
+			int correctIdx = nums[i]; //where this element should be in sorted array
+            if(correctIdx<n && correctIdx != i) //if not already at correct position and correct position < n
+            {
+                swap(nums, i,correctIdx); //put current element at correct position
+            } 
+			else
+				i++; // move to next index
+        }
+        for(i=0;i<n; i++)
+            if(nums[i]!=i)
+                return i;
+        return n;
         
     }
 }
