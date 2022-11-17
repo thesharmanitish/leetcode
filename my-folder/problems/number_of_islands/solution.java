9c91256626797c672e20c6680007d7b5248e15c1
Nitish Sharma <nitss007@gmail.com>
2022-11-17 08:20:50 +0000

    Sync LeetCode submission - Number of Islands (java)

--- my-folder/problems/number_of_islands/solution.java
+++ my-folder/problems/number_of_islands/solution.java
@@ -1,29 +1,28 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        // boolean[][] visited  = new boolean[grid.length][grid[0].length];
-        if (grid == null || grid.length == 0) {
-          return 0;
-        }        
-        int count = 0;
+        int sum = 0;
         for(int i=0;i<grid.length;i++){
             for(int j=0;j<grid[0].length;j++){
-                if(grid[i][j] == '0')
+                if(grid[i][j] =='0')
                     continue;
-                dfs(grid, i, j);
-                count+=1;
+                sum +=1;    
+                dfs(grid,i, j);
             }
         }
-        return count;
+        return sum;
     }
+    static int[] row = {-1,1,0,0};
+    static int[] col = {0,0,-1,1};
+    
     public void dfs(char[][] grid, int i, int j) {
-        if(i<0 || j<0 || i>=grid.length || j>= grid[0].length|| grid[i][j] =='0')
+        if(i<0 ||j<0 || i>=grid.length || j>= grid[0].length || grid[i][j] =='0')
             return;
+        
         grid[i][j] = '0';
         
-        dfs(grid, i+1, j );
-        dfs(grid, i-1, j );
-        dfs(grid, i, j+1 );
-        dfs(grid, i, j-1 );
-            
+        for(int x =0;x<4;x++){
+            dfs(grid,i+row[x], j+col[x]);
+        }
+
     }
 }
