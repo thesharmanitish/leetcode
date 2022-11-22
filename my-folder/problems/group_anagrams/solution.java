b482de4ada668c96e2d23d766b15d63f4e2f4a1f
Nitish Sharma <nitss007@gmail.com>
2022-11-22 11:42:24 +0000

    Sync LeetCode submission - Group Anagrams (java)

--- my-folder/problems/group_anagrams/solution.java
+++ my-folder/problems/group_anagrams/solution.java
@@ -1,32 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<Long, List<String>> map = new HashMap<>();
-        for(String str:strs){
-            long sum=0;
-            for(char c:str.toCharArray()){
-                sum+=(long) c*(c-1)*(c*c+1)*(7919);
-            }
-            map.computeIfAbsent(sum, (s)->new ArrayList<>()).add(str);
+        // Map<Long, List<String>> map = new HashMap<>();
+        // for(String str:strs){
+        //     long sum=0; //(long)1<<31;
+        //     sum-=1;
+        //     for(char c:str.toCharArray()){
+        //         // sum += sum*31 + c;
+        //         sum+=(long) (c*(c-1)+1<<c)*(1<<31);
+        //     }
+        //     map.computeIfAbsent(sum, (s)->new ArrayList<>()).add(str);
+        // }
+        
+        // return map.values().stream().collect(Collectors.toList());
+        
+        Map<String,List<String>> map = new HashMap<>();
+        
+        for(String s:strs){
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String t = String.valueOf(arr);
+            map.computeIfAbsent(t, (x)->new ArrayList<>()).add(s);
         }
-        
-        return map.values().stream().collect(Collectors.toList());
-        
-//         Map<String,List<String>> map = new HashMap<>();
-        
-//         for(String s:strs){
-//             char[] arr = s.toCharArray();
-//             Arrays.sort(arr);
-//             String t = String.valueOf(arr);
-//             if(map.get(t)!=null){
-//                 List<String> x = map.getOrDefault(t, new ArrayList<>());
-//                 x.add(s);
-//                 map.put(t,x);
-//             }else{
-//                 List<String> x = new ArrayList<>();
-//                 x.add(s);
-//                 map.put(t, x);
-//             }
-//         }
-//         return map.values().stream().collect(Collectors.toList());        
+        return map.values().stream().collect(Collectors.toList());        
     }
 }
