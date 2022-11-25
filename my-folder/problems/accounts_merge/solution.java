76cd6476b646e016709f1893295239d6d291a012
Nitish Sharma <nitss007@gmail.com>
2022-11-25 18:41:30 +0000

    Sync LeetCode submission - Accounts Merge (java)

--- my-folder/problems/accounts_merge/solution.java
+++ my-folder/problems/accounts_merge/solution.java
@@ -1,66 +1,68 @@
 class Solution {
-    HashSet<String> visited = new HashSet<>();
-    Map<String, List<String>> adjacent = new HashMap<String, List<String>>();
-    
-    private void DFS(List<String> mergedAccount, String email) {
-        visited.add(email);
-        // Add the email vector that contains the current component's emails
-        mergedAccount.add(email);
-        
-        if (!adjacent.containsKey(email)) {
-            return;
+    int[] root;
+    int[] rank;
+
+	// The find function here is the same as that in the disjoint set with path compression.
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
         }
-        
-        for (String neighbor : adjacent.get(email)) {
-            if (!visited.contains(neighbor)) {
-                DFS(mergedAccount, neighbor);
+        return root[x] = find(root[x]);
+    }
+
+	// The union function with union by rank
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
             }
         }
     }
-    
-    public List<List<String>> accountsMerge(List<List<String>> accountList) {
-        int accountListSize = accountList.size();
-        
-        for (List<String> account : accountList) {
-            int accountSize = account.size();
-            
-            // Building adjacency list
-            // Adding edge between first email to all other emails in the account
-            String accountFirstEmail = account.get(1);
-            for (int j = 2; j < accountSize; j++) {
-                String accountEmail = account.get(j);
-                
-                if (!adjacent.containsKey(accountFirstEmail)) {
-                    adjacent.put(accountFirstEmail, new ArrayList<String>());
-                }
-                adjacent.get(accountFirstEmail).add(accountEmail);
-                
-                if (!adjacent.containsKey(accountEmail)) {
-                    adjacent.put(accountEmail, new ArrayList<String>());
-                }
-                adjacent.get(accountEmail).add(accountFirstEmail);
-            }
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        root = new int[accounts.size()];
+        rank = new int[accounts.size()];
+
+        for(int i=0;i<root.length;i++){
+            root[i] = i;
+            rank[i] = 1;
         }
-        
-        // Traverse over all th accounts to store components
-        List<List<String>> mergedAccounts = new ArrayList<>();
-        for (List<String> account : accountList) {
-            String accountName = account.get(0);
-            String accountFirstEmail = account.get(1);
-            
-            // If email is visited, then it's a part of different component
-            // Hence perform DFS only if email is not visited yet
-            if (!visited.contains(accountFirstEmail)) {
-                List<String> mergedAccount = new ArrayList<>();
-                // Adding account name at the 0th index
-                mergedAccount.add(accountName);
-                
-                DFS(mergedAccount, accountFirstEmail);
-                Collections.sort(mergedAccount.subList(1, mergedAccount.size())); 
-                mergedAccounts.add(mergedAccount);
+        Map<String, Integer> map = new HashMap<>();
+        int i=0;
+        for(List<String> account: accounts){
+            for(int j=1;j<account.size();j++){
+                String email = account.get(j);
+                if(!map.containsKey(email))
+                    map.put(email, i);
+                else{
+                    union(map.get(email), i);
+                }
             }
+            i++;
         }
-        
-        return mergedAccounts;
+        List<List<String>> result = new ArrayList<>();
+
+        Map<Integer, List<String>> resultMap = new HashMap<>();
+        for(Map.Entry<String, Integer> entry:map.entrySet()){
+            resultMap.computeIfAbsent(find(entry.getValue()), ArrayList::new).add(entry.getKey());
+        }
+
+        for(Map.Entry<Integer, List<String>> entry:resultMap.entrySet()){
+            List<String> ll = new ArrayList<>();
+            
+            ll.addAll(entry.getValue());
+            Collections.sort(ll);
+            ll.add(0, accounts.get(entry.getKey()).get(0));
+            result.add(ll);
+        }
+        return result;
+
     }
 }
