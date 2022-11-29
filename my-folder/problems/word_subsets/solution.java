cfc01a7cce53d8d08d20cd97bf75f4b1218978ba
Nitish Sharma <nitss007@gmail.com>
2022-11-29 11:29:07 +0000

    Sync LeetCode submission - Word Subsets (java)

--- my-folder/problems/word_subsets/solution.java
+++ my-folder/problems/word_subsets/solution.java
@@ -1,28 +1,28 @@
 class Solution {
     public List<String> wordSubsets(String[] A, String[] B) {
-        int[] bmax = count("");
-        for (String b: B) {
-            int[] bCount = count(b);
-            for (int i = 0; i < 26; ++i)
-                bmax[i] = Math.max(bmax[i], bCount[i]);
+        List<String> result = new ArrayList<>();
+        
+        int[] arr = new int[26];
+        int[] brr;
+        for (String str: B){
+            brr = new int[26];
+            for(char c:str.toCharArray())
+                brr[c - 'a']++;
+            for(int i=0;i<26;i++){
+                arr[i] = Math.max(arr[i], brr[i]);
+            }
         }
-
-        List<String> ans = new ArrayList();
-        search: for (String a: A) {
-            int[] aCount = count(a);
-            for (int i = 0; i < 26; ++i)
-                if (aCount[i] < bmax[i])
-                    continue search;
-            ans.add(a);
+        
+        for(String str:A){
+            brr = new int[26];
+            for(char c:str.toCharArray()){
+                if(arr[c - 'a']!=0 && arr[c - 'a']> brr[c-'a'])
+                    brr[c-'a']++;
+            }
+            if(Arrays.equals(arr,brr))
+                result.add(str);
         }
+        return result;
 
-        return ans;
-    }
-
-    public int[] count(String S) {
-        int[] ans = new int[26];
-        for (char c: S.toCharArray())
-            ans[c - 'a']++;
-        return ans;
     }
 }
