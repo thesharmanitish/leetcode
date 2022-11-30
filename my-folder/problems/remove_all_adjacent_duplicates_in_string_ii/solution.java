5a360ddcfeaf3b37f2aa3e180bc9d8d3ad642174
Nitish Sharma <nitss007@gmail.com>
2022-11-30 17:00:56 +0000

    Sync LeetCode submission - Remove All Adjacent Duplicates in String II (java)

--- my-folder/problems/remove_all_adjacent_duplicates_in_string_ii/solution.java
+++ my-folder/problems/remove_all_adjacent_duplicates_in_string_ii/solution.java
@@ -1,23 +1,40 @@
 class Solution {
     public String removeDuplicates(String s, int k) {
-        if(s.length()<k || k<2)
-            return s;
         StringBuilder sb = new StringBuilder(s);
-        int count[] = new int[sb.length()];
-        count[0] = 1;
+        char prev = ' ';
+        int n =sb.length() , count = 0;
         for(int i=0;i<sb.length();i++){
-            if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
-                 count[i] = 1;
-             }
-            else{
-                count[i] = count[i-1]+1;
-                if(count[i]==k){
-                    sb.delete(i-k+1, i+1);
-                    i = i-k;    
+            if(prev == sb.charAt(i)){
+                count += 1;
+                if(count == k){
+                    prev = ' ';
+                    while(count-->0)
+                        sb.deleteCharAt(i--);
+                    i = Math.max(-1, i-k);
                 }
+            }else{
+                count = 1;
+                prev = sb.charAt(i);
             }
             
         }
+        
         return sb.toString();
     }
+    // public String removeDuplicates(String s, int k) {
+    //     StringBuilder sb = new StringBuilder(s);
+    //     int count[] = new int[sb.length()];
+    //     for (int i = 0; i < sb.length(); ++i) {
+    //         if (i == 0 || sb.charAt(i) != sb.charAt(i - 1)) {
+    //             count[i] = 1;
+    //         } else {
+    //             count[i] = count[i - 1] + 1;
+    //             if (count[i] == k) {
+    //                 sb.delete(i - k + 1, i + 1);
+    //                 i = i - k;
+    //             }
+    //         }
+    //     }
+    //     return sb.toString();
+    // }    
 }
