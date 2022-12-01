770caae3a4afb466849269111ee679d3cf637180
Nitish Sharma <nitss007@gmail.com>
2022-12-01 15:55:59 +0000

    Sync LeetCode submission - Trapping Rain Water (java)

--- my-folder/problems/trapping_rain_water/solution.java
+++ my-folder/problems/trapping_rain_water/solution.java
@@ -1,26 +1,41 @@
 class Solution {
     public int trap(int[] height) {
-        int start =0, end = height.length-1, sum = 0;
-        int maxLeft = height[start], maxRight = height[end];
-        while(start<end){
-            if(height[start]<height[end]){
-                if(maxLeft<height[start]){
-                    maxLeft = height[start];
-                    
-                }
-                
-                sum+= maxLeft - height[start];
-                start++;
-            }else{
-                if(maxRight<height[end]){
-                    maxRight = height[end];
-                    
-                }
-                
-                sum+= maxRight - height[end];                
-                end--;
-            }
-        }
-        return sum;
+        int start =0 , end = height.length-1, leftMax=height[0], rightMax = height[end];
+        int sum = 0;
+		while(start<end){
+			if(height[start]<height[end]){
+                if(leftMax < height[start])
+                    leftMax = height[start];
+                sum += leftMax- height[start];
+                // rightMax = Math.max(rightMax, height[end]);
+				start++;
+			}else{
+                if(rightMax < height[end])
+                    rightMax = height[end];
+				sum += rightMax- height[end];
+				end--;
+			}
+		}
+		return sum;        
     }
 }
+        // while(start<end){
+        //     if(height[start]<height[end]){
+        //         if(maxLeft<height[start]){
+        //             maxLeft = height[start];
+                    
+        //         }
+                
+        //         sum+= maxLeft - height[start];
+        //         start++;
+        //     }else{
+        //         if(maxRight<height[end]){
+        //             maxRight = height[end];
+                    
+        //         }
+                
+        //         sum+= maxRight - height[end];                
+        //         end--;
+        //     }
+        // }
+        // return sum;
