6a037a8dcca5521c838747a825177063ccb65d18
Nitish Sharma <nitss007@gmail.com>
2022-12-01 10:56:27 +0000

    Sync LeetCode submission - Min Cost to Connect All Points (java)

--- my-folder/problems/min_cost_to_connect_all_points/solution.java
+++ my-folder/problems/min_cost_to_connect_all_points/solution.java
@@ -1,38 +1,39 @@
-class Edge{
+
+class Solution {
+    class Edge{
     int[] nodeA;
     int[] nodeB;
     int distance;
-    int a;
     int b;
-    public Edge(int[] nodeA, int[] nodeB, int a, int b){
+    public Edge(int[] nodeA, int[] nodeB,int b){
         this.nodeA = nodeA;
         this.nodeB = nodeB;
-        this.a = a;
         this.b = b;
         distance = Math.abs(nodeA[0]-nodeB[0])+ Math.abs(nodeA[1]-nodeB[1]);
+        }
     }
-}
-class Solution {
     public int minCostConnectPoints(int[][] points) {
 
         PriorityQueue<Edge> pq = new PriorityQueue<>((a,b)-> a.distance - b.distance);
 
         for(int i=1;i<points.length;i++){
-            pq.add(new Edge(points[0], points[i], 0, i));
+            pq.add(new Edge(points[0], points[i], i));
         }
-        int count = 0, sum = 0, n = points.length-1;
+        int sum = 0, n = points.length-1;
         boolean[] visited = new boolean[points.length];
         visited[0] = true;
-        while(count < n){
+        for(int count = 0;count<points.length-1;){
             Edge edge = pq.poll();
             if(visited[edge.b])
                 continue;
             sum += edge.distance;
-            System.out.println("sum = "+sum+ " a: "+ edge.a + " b:"+edge.b);
             visited[edge.b] = true;
+
             for(int i=1;i<=n;i++){
                 if(!visited[i])
-                    pq.add(new Edge(edge.nodeB, points[i], edge.b, i));
+                    pq.add(new Edge(edge.nodeB, points[i], i));
+                if(!pq .isEmpty() &&  visited[pq.peek().b])
+                    pq.poll();                    
             }
             count++;
         }
