8637be501dddf5271e64ba7ae731da6da183fb1e
Nitish Sharma <nitss007@gmail.com>
2022-12-02 09:41:59 +0000

    Sync LeetCode submission - String to Integer (atoi) (java)

--- my-folder/problems/string_to_integer_(atoi)/solution.java
+++ my-folder/problems/string_to_integer_(atoi)/solution.java
@@ -1,49 +1,43 @@
 class Solution {
     public int myAtoi(String s) {
         s = s.trim();
-        if(s== null || s.length() ==0)
+        if(s.length() ==0)
             return 0;
+        if(s.length()==1)
+            return Character.isDigit(s.charAt(0))?s.charAt(0)-'0':0;
+        StringBuilder result = new StringBuilder();
+        boolean flag = true;
+        char sign = ' ';
         int i=0;
-        Boolean neg =null;
-        for(char ch:s.toCharArray()){
+        while(i<s.length() && Character.isSpace(s.charAt(i)))
             i++;
-            if(ch=='-')  {
-                if(neg !=null)
-                    return 0;
-                neg = true;
-            }
-            else if(ch=='+'){
-                if(neg !=null)
-                    return 0;
-                neg = false;
-            }
-            else if(ch>='0' && ch<='9')
-                break;
-            else if(ch ==' '){
-                if(neg!=null)
-                    return 0;
-                continue;
-            }
-            else if(ch=='0')
-                continue;
-            else 
-                return 0;
-            
+        if(i<s.length() && (s.charAt(i) =='-' || s.charAt(i) =='+' )){
+            sign = s.charAt(i);
+            i++;
         }
-        long  sum=0;
-        if(neg ==null)
-            neg = false;        
-        for(int j=i-1;j<s.length();j++){
-            if(s.charAt(j) >='0' && s.charAt(j)<='9'){
-                int diff = s.charAt(j)-'0';
-                sum = sum*10+diff;
-                
-                if(sum>Integer.MAX_VALUE)
-                    return neg?Integer.MIN_VALUE:Integer.MAX_VALUE;
-            }
-            else break;
-        }
-        
-        return neg?(int)-sum:(int)sum;
+        if(!Character.isDigit(s.charAt(i)))
+            return 0;
+        if(sign=='-')
+            result.append('-');
+        while(i<s.length() && s.charAt(i) == '0')
+            i++;
+
+        if(i<s.length() && !Character.isDigit(s.charAt(i)))
+            return 0;            
+        while(i<s.length() && Character.isDigit(s.charAt(i)))
+            result.append(s.charAt(i++));
+        if(result.length()<1)
+            return 0;
+ 
+        else if(result.length()>11) 
+            if( sign == '-')
+                return Integer.MIN_VALUE;
+            else return Integer.MAX_VALUE;
+        else if(Long.parseLong(result.toString())<= Integer.MIN_VALUE )
+            return Integer.MIN_VALUE; 
+        else if(Long.parseLong(result.toString())>= Integer.MAX_VALUE)
+            return Integer.MAX_VALUE;
+        else return Integer.parseInt(result.toString());    
+
     }
 }
