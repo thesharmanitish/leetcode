8482f1dfff8550238fd4dacf746db0559db4fd52
Nitish Sharma <nitss007@gmail.com>
2022-12-03 20:38:56 +0000

    Sync LeetCode submission - Roman to Integer (java)

--- my-folder/problems/roman_to_integer/solution.java
+++ my-folder/problems/roman_to_integer/solution.java
@@ -1,55 +1,34 @@
 class Solution {
     public int romanToInt(String s) {
+        StringBuilder result = new StringBuilder();
+        if(s.length()==1)
+            evaluate(s.charAt(0));
+        int sum = 0;
+        int prev = -1;
+        for(int i=s.length()-1;i>=0;i--){
+            int cur = evaluate(s.charAt(i));
+            if(cur<prev){
+                sum-= cur;
+            }else
+                sum+=cur;
+            
+            prev = cur;
+        }
+        return sum;
+    }
+    public int evaluate(char c){
         int result = 0;
-        for(int i=0 ;i<s.length();i++){
-            switch(s.charAt(i)){
-                case 'M': result += 1000; break;
-                case 'D': result += 500;break;
-                case 'C': if(i+1>= s.length())
-                                result+=100;
-                            else if(s.charAt(i+1) == 'D'){
-                                result +=400;
-                                i++;
-                            }
-                           else if(s.charAt(i+1) == 'M'){
-                                 result +=900;
-                                 i++;
-                            }
-                          else result+=100;
-                          break;  
-                case 'L': result+=50;
-                          break;
-                case 'X':  
-                            if(i+1>= s.length())
-                                result+=10;
-                            else if(s.charAt(i+1) == 'L'){
-                                result +=40;
-                                i++;
-                            }
-                           else if(s.charAt(i+1) == 'C'){
-                                 result +=90;
-                                 i++;
-                            }
-                          else result+=10;
-                          break;
-                case 'V': result += 5; break;    
-                case 'I': 
-                            if(i+1>=s.length())
-                                result+=1;
-                            else if(s.charAt(i+1) == 'V'){
-                                result +=4;
-                                i++;
-                            }
-                           else if(s.charAt(i+1) == 'X'){
-                                 result +=9;
-                                 i++;
-                            }else
-                               result +=1;
-                          
-                          break;    
-            }
+        switch(c){
+            case 'I': result = 1; break;
+            case 'V': result = 5; break;
+            case 'X': result = 10; break;
+            case 'L': result = 50; break;
+            case 'C': result = 100; break;
+            case 'D': result = 500; break;
+            case 'M': result = 1000; break;
         }
         return result;
-            
+
     }
+
 }
