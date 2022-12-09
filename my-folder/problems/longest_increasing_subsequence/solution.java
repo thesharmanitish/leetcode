bc88743e99e866b1b799f06c01117fff351b4e24
Nitish Sharma <nitss007@gmail.com>
2022-12-09 13:22:43 +0000

    Sync LeetCode submission - Longest Increasing Subsequence (java)

--- my-folder/problems/longest_increasing_subsequence/solution.java
+++ my-folder/problems/longest_increasing_subsequence/solution.java
@@ -1,45 +1,27 @@
 class Solution {
     public int lengthOfLIS(int[] nums) {
-//         int[] dp = new int[nums.length];
-        
-//         Arrays.fill(dp,1);
-//         int max = dp[0];
-        
-//         for(int i=1;i<nums.length;i++){
-//             for(int j=0;j<i;j++){
-//                 if(nums[i]>nums[j])
-//                     dp[i] = Math.max(dp[i], dp[j]+1);
-//             }
-//             max = Math.max(max, dp[i]);
-//         }
-//         return max;
-        
-        
-//         int[] tails = new int[nums.length];
-// 	    int size = 0;
-// 	    for(int num:nums) {
-// 	    	int i=0, j= size;
-// 	    	while(i<j) {
-// 	    		int mid = (i+j)/2;
-// 	    		if(num>tails[mid]) {
-// 	    			i = mid+1;
-// 	    		}else {
-// 	    			j= mid;
-// 	    		}
-// 	    	}
-// 	    	if(i==size) size++;
-// 	    	tails[i] = num;
-	    
-// 	    }
-// 	    return size;
-        
-        TreeSet<Integer> set = new TreeSet<>();
-        for(int num:nums){
-            Integer x = set.ceiling(num);
-            if(x!=null)
-                set.remove(x);
-            set.add(num);
+        // TreeSet<Integer> tset = new TreeSet<>();
+
+        // for(int num:nums){
+        //     Integer x = tset.ceiling(num);
+        //     if(x!=null)
+        //         tset.remove(x);
+        //     tset.add(num);
+        // }
+        // return tset.size();
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int max=1;
+        for(int i=1;i<nums.length;i++){
+            for(int j=0;j<=i;j++){
+                if(nums[i]>nums[j]){
+                    dp[i] =Math.max(dp[i],1+dp[j]);
+                    max = Math.max(max, dp[i]);
+                }
+            }
         }
-        return set.size();
+        return max;
+
+
     }
 }
