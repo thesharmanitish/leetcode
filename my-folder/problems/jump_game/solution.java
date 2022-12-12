9654cbf6ecffbcf567cae053ed91780f6ba9b0d3
Nitish Sharma <nitss007@gmail.com>
2022-12-12 19:13:20 +0000

    Sync LeetCode submission - Jump Game (java)

--- my-folder/problems/jump_game/solution.java
+++ my-folder/problems/jump_game/solution.java
@@ -1,69 +1,18 @@
-enum Index {
-    GOOD, BAD, UNKNOWN
-}
 class Solution {
-    Index[] memo;
-    int[] dp;
-    public boolean canJumpFromPosition(int position, int[] nums) {
-        if (memo[position] != Index.UNKNOWN) {
-            return memo[position] == Index.GOOD ? true : false;
-        }
-        int furthestJump = Math.min(nums[position]+position,nums.length-1);
-        for (int nextPosition = position + 1; nextPosition <= furthestJump; nextPosition++) {
-            if(canJumpFromPosition(nextPosition, nums)){
-                 memo[position] = Index.GOOD;
-                return true;
-            }
-        }
-        memo[position] = Index.BAD;
-        return false;
-    }
-    
     public boolean canJump(int[] nums) {
-        memo = new Index[nums.length];
-        for (int i = 0; i < memo.length; i++) {
-            memo[i] = Index.UNKNOWN;
+        if(nums.length <2)
+            return true;
+        int x=0;
+        boolean flag = true;
+        for(int i= nums.length-2;i>=0;i--){
+            flag = false;
+            if(nums[i]>x){
+                x=0;
+                flag = true;
+            }
+            else
+                x++;
         }
-        memo[memo.length - 1] = Index.GOOD;        
-        return canJumpFromPosition(0, nums);
-        
-        
-        
-        // int len = nums.length;
-        // int max = 0;
-        // for(int i=0; i<=max; i++){
-        //     max = Math.max(max, i+nums[i]);
-        //     if(max >= len-1)  return true;
-        // }
-        // return false;
-        
-        
-        
-//         if(nums.length <=1)
-//             return true;
-//         if(nums[0] ==0)
-//             return false;
-//         boolean flag =false;
-//         for(int i=nums.length-1;i>0;i--){
-//             if(nums[i]!=0)
-//                 continue;
-//             int t = 1;
-//             flag  = true;
-//             int prev = i;
-//             while(--i>=0){
-//                 if(nums[i]>t){
-//                     flag = false;
-//                     break;
-//                 }else if(nums[i]==t &&  prev == nums.length-1){
-//                     flag = false;
-//                     break;
-//                 }
-//                 t++;
-//             }
-//             if(flag)
-//                 return false;
-//         }
-        
-//             return true;
+        return flag;
     }
 }
