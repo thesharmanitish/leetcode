e19ba20a8ba82cbaf6c7f44d2edfd2bd2b154158
Nitish Sharma <nitss007@gmail.com>
2022-12-15 16:53:35 +0000

    Sync LeetCode submission - Longest Common Subsequence (java)

--- my-folder/problems/longest_common_subsequence/solution.java
+++ my-folder/problems/longest_common_subsequence/solution.java
@@ -1,22 +1,24 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        if(text1.length() ==1 )
-            return text2.indexOf(text1) >=0 ?1:0;
-        if(text2.length() ==1)
-            return text1.indexOf(text2.charAt(0)) >= 0?1:0;
-        dp = new int[text1.length()+1][text2.length()+1];
-        return longestCommonSubsequence(text1, text2, 0, 0);
-        
-    }
     int[][] dp;
-    public int longestCommonSubsequence(String text1, String text2, int i, int j) {
-        if(i>=text1.length() || j>= text2.length())
+    public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length(), n = text2.length();
+        if(m<n)
+            longestCommonSubsequence(text2, text1);
+
+        if(text2.length() ==1 )
+            return text1.indexOf(text2) >=0 ?1:0;        
+        dp = new int[m+1][n+1];
+        return longestCommonSubsequenceRec(text1.toCharArray(), text2.toCharArray(), 0, 0);
+
+    }
+    public int longestCommonSubsequenceRec(char[] text1, char[] text2, int i, int j){
+        if(i>= text1.length || j>= text2.length)
             return 0;
-        if(dp[i][j]!=0)
+        if(dp[i][j]>0)
             return dp[i][j];
-        if(text1.charAt(i) == text2.charAt(j))
-            return dp[i][j] = 1+ longestCommonSubsequence(text1, text2, i+1, j+1);
-        else
-            return dp[i][j] = Math.max(longestCommonSubsequence(text1, text2, i+1, j), longestCommonSubsequence(text1, text2, i, j+1));
+        if(text1[i] == text2[j])
+            return dp[i][j] = 1+longestCommonSubsequenceRec(text1, text2, i+1, j+1);
+
+        return dp[i][j] = Math.max(longestCommonSubsequenceRec(text1, text2, i+1, j),longestCommonSubsequenceRec(text1, text2, i, j+1));
     }
 }
