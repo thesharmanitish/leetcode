f0bb195fbae6c25edc59481fe73d0b4b710a9868
Nitish Sharma <nitss007@gmail.com>
2022-12-17 17:49:19 +0000

    Sync LeetCode submission - Next Permutation (java)

--- my-folder/problems/next_permutation/solution.java
+++ my-folder/problems/next_permutation/solution.java
@@ -1,37 +1,38 @@
 class Solution {
     public void nextPermutation(int[] nums) {
-        int pivot = findPeek(nums)-1;
-        if(pivot!=-1){
-            int x = findNextBig(nums, nums[pivot]);
-            swap(nums, x, pivot)    ;
-            
+        int i=0;
+        for( i=0;i<nums.length-1;i++){
+            if(nums[i]<nums[i+1])
+               break;
         }
-        reverseSuffix(nums,pivot+1);
-        // Arrays.stream(nums).forEach(System.out::print);
-    }
-    public int  findPeek(int[] nums){
-        for(int i=nums.length-1;i>0;i--)
-            if(nums[i-1]<nums[i])
-                return i;
-        return 0;
-    }
-     public int  findNextBig(int[] nums, int num){
-         for(int i=nums.length-1;i>=0;i--)
-            if(nums[i]>num)
-                return i;
-        return -1;
-     }
-    private void swap(int[] A, int i, int j) {
-        int tmp = A[i];
-        A[i] = A[j];
-        A[j] = tmp;
+        if(i== nums.length-1){
+            Arrays.sort(nums);
+            return;
+        }
+        for(i=nums.length-1;i>0;i--){
+            if(nums[i]> nums[i-1]){
+                int start =i;
+                while(start<nums.length && nums[start] >nums[i-1])
+                    start++;                
+                swap(nums, i-1, start-1);
+                reverse(nums, i);   
+                return;
+            }
+        }
     }
 
-    /** Reverse numbers starting from an index till the end. */
-    void reverseSuffix(int[] nums, int start) {
-        int end = nums.length - 1;
-        while (start < end) {
-            swap(nums, start++, end--);
+    private void reverse(int[] nums, int i) {
+        int j = nums.length - 1;
+        while (i < j) {
+            swap(nums, i, j);
+            i++;
+            j--;
         }
-    }
+    } 
+    
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }       
 }
