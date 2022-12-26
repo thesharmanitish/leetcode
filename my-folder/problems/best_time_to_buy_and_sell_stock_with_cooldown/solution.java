f2d566e4a0240645bbd18a828d6c39cd7f6a548f
Nitish Sharma <nitss007@gmail.com>
2022-12-26 12:59:46 +0000

    Sync LeetCode submission - Best Time to Buy and Sell Stock with Cooldown (java)

--- my-folder/problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.java
+++ my-folder/problems/best_time_to_buy_and_sell_stock_with_cooldown/solution.java
@@ -2,13 +2,10 @@ class Solution {
     public int maxProfit(int[] prices) {
         int[] dp = new int[prices.length+2];
         int n = prices.length;
-        int max =0;
-        for(int i=n-1;i>=0;i--){
-            for(int j=i+1;j<n;j++){
-                
+        for(int i=n-2;i>=0;i--){
+            for(int j=i+1;j<n;j++)
                 dp[i] = Math.max(prices[j]-prices[i]+dp[j+2],dp[i]);
-                // max = Math.max(max, dp[i]);
-            }
+
             dp[i] = Math.max(dp[i], dp[i+1]);
             
         }
