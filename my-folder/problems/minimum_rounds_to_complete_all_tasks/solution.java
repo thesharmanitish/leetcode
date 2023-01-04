5c725428e689ddb40e8b21b901201f9ae7489598
Nitish Sharma <nitss007@gmail.com>
2023-01-04 06:18:34 +0000

    Sync LeetCode submission - Minimum Rounds to Complete All Tasks (java)

--- my-folder/problems/minimum_rounds_to_complete_all_tasks/solution.java
+++ my-folder/problems/minimum_rounds_to_complete_all_tasks/solution.java
@@ -1,25 +1,15 @@
 class Solution {
     public int minimumRounds(int[] tasks) {
-        Map<Integer, Integer> map = new HashMap<>();
+        Map<Integer, Integer> taskMap = new HashMap<>();
 
         for(int task:tasks){
-            map.put(task, map.getOrDefault(task, 0)+1);
+            taskMap.put(task, taskMap.getOrDefault(task, 0)+1);
         }
         int count = 0;
-        for(int v:map.values()){
-            if(v<2)
+        for(int val:taskMap.values()){
+            if(val == 1)
                 return -1;
-            
-            else if(v>=3){
-                
-                count+=v/3;
-                if(v%3>0)
-                    count+=1;
-            }else if(v==2){
-                count += 1;
-            }
-            
-
+            count += Math.ceil(val/3.0);
         }
         return count;
     }
