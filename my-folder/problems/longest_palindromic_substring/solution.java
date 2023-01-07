eb2ff3776849f351fea7d9646b9d33cdde30c16e
Nitish Sharma <nitss007@gmail.com>
2023-01-07 17:29:26 +0000

    Sync LeetCode submission - Longest Palindromic Substring (java)

--- my-folder/problems/longest_palindromic_substring/solution.java
+++ my-folder/problems/longest_palindromic_substring/solution.java
@@ -6,11 +6,10 @@
         
         int n = arr.length, max = -1;
         
-        boolean[][] dp = new boolean[arr.length+1][arr.length+1];
+        boolean[][] dp = new boolean[arr.length][arr.length];
         int start =0, end =0;
-        for(int i=0;i<n;i++){
-            dp[i][i] = true;
-            for(int j=0;j<=i;j++){
+        for(int i=1;i<arr.length;i++){
+            for(int j=0;j<i;j++){
                 if(arr[i]==arr[j]&& (i-j<3 || dp[i-1][j+1])){
                     dp[i][j] = true;
                     if(max<i-j+1){
