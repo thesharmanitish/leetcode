981b41242e4308fd7ca8f9520b9303003401a7ab
Nitish Sharma <nitss007@gmail.com>
2023-01-11 16:52:53 +0000

    Sync LeetCode submission - Isomorphic Strings (java)

--- my-folder/problems/isomorphic_strings/solution.java
+++ my-folder/problems/isomorphic_strings/solution.java
@@ -4,10 +4,6 @@ class Solution {
         int[] tarr = new int[254];
         int prev = 0;
         for(int i=0;i<s.length();i++){
-            // if(s.charAt(i)-'a' !=prev){
-            //     sarr[prev] =0;
-            //     tarr[prev] =0;
-            // }
             if(sarr[s.charAt(i)] != tarr[t.charAt(i)])
                 return false;            
             sarr[s.charAt(i)] =i+1;
@@ -17,3 +13,27 @@ class Solution {
         return true;
     }
 }
+// class Solution {
+//     public boolean isIsomorphic(String s, String t) {
+//         if(s.length()!= t.length())
+//             return false;
+//         Map<Character, Integer> map = new HashMap<>();
+//         int n = s.length();
+//         boolean[] visited = new boolean[128];
+//         for(int i=0;i<n;i++){
+//             char c = s.charAt(i);
+//             if(!map.containsKey(c)){
+//                 if(visited[t.charAt(i)])
+//                     return false;
+//                 map.put(c, i);
+//                 visited[t.charAt(i)] = true;
+//             }
+//             else{
+//                 int ind = map.get(c);
+//                 if(t.charAt(ind)!=t.charAt(i))
+//                     return false;
+//             }
+//         }
+//         return true;
+//     }
+// }
