00eb4a746010f4880a87ea58ab0d84f31d80eb87
Nitish Sharma <nitss007@gmail.com>
2023-01-14 10:16:06 +0000

    Sync LeetCode submission - Longest Palindrome (java)

--- my-folder/problems/longest_palindrome/solution.java
+++ my-folder/problems/longest_palindrome/solution.java
@@ -1,15 +1,17 @@
 class Solution {
     public int longestPalindrome(String s) {
-        if(s.length() <=1)
-            return s.length();
-        int[] freq =   new int[128];
+        int[] freq = new int[128];
         for(char c:s.toCharArray()){
             freq[c] +=1;
         }
+        boolean isSingle = false;
         int count = 0;
-        for(int f:freq){
-            count+=(f/2)*2;
+        for(int i=0;i<128;i++){
+            if(!isSingle && freq[i]%2==1)
+                isSingle = true;
+            if(freq[i]>1)
+                count+=(freq[i]/2);
         }
-        return s.length()==count?count:count+1;
+        return isSingle?(count*2)+1:(count*2);
     }
 }
