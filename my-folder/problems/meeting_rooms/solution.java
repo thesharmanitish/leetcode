647f4bb8b023861cc40bfd353546c2fdcb347d38
Nitish Sharma <nitss007@gmail.com>
2023-01-16 11:40:16 +0000

    Sync LeetCode submission - Meeting Rooms (java)

--- my-folder/problems/meeting_rooms/solution.java
+++ my-folder/problems/meeting_rooms/solution.java
@@ -1,9 +1,11 @@
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> {return a[0] - b[0];});
-        for(int i=1;i<intervals.length;i++){
-            if(intervals[i-1][1]> intervals[i][0])
+        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
+        int[] prev = new int[]{-1,-1};
+        for(int[] interval:intervals){
+            if(interval[0] <prev[1])
                 return false;
+            prev = interval; 
         }
         return true;
     }
