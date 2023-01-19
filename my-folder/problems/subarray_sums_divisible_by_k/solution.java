41fabac11b114ed7e7f8244f93048c3b3152377c
Nitish Sharma <nitss007@gmail.com>
2023-01-19 17:27:52 +0000

    Sync LeetCode submission - Subarray Sums Divisible by K (java)

--- my-folder/problems/subarray_sums_divisible_by_k/solution.java
+++ my-folder/problems/subarray_sums_divisible_by_k/solution.java
@@ -1,38 +1,15 @@
 class Solution {
-//     public int subarraysDivByK(int[] nums, int k) {
-//         if(nums.length ==1){
-//             return nums[0]%k==0?1:0;
-//         }
-//         int[] dp = new int[nums.length];
-//         int count = 0;
-        
-//         for(int i=1;i<=nums.length;i++){
-//             dp[i-1] = nums[i-1];
-//              if(dp[i-1]%k ==0){
-//                     count++;
-//                 }
-//             for(int j=i;j<nums.length;j++){
-//                 dp[j] = dp[j-1]+nums[j];
-//                 if(dp[j]%k ==0){
-//                     count++;
-//                 }
-//             }
-//         }
-//         return count;
-//     }
-    
-    
     public int subarraysDivByK(int[] nums, int k) {
-        
-        int[] dp = new int[k];
-        int sum = 0;
-        for(int n:nums){
-            sum += ((n%k)+k)%k;
-            dp[sum%k]++;
-        }
-        int count = dp[0];
-        for(int d:dp){
-            count += (d * (d-1))/2;
+
+        int count = 0, prefixSum = 0;
+        int[] res =new int[k];
+        res[0] = 1;
+        for(int i=0;i<nums.length;i++){
+            prefixSum += nums[i];
+            int remainder = ((prefixSum % k)+k)%k;
+            count+=res[remainder];
+            res[remainder] +=1;
+            
         }
         return count;
     }
