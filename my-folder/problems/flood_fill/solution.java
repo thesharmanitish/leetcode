293cc5701bd1c0cbfe9d9d4b0862e2a040e473ac
Nitish Sharma <nitss007@gmail.com>
2023-01-22 18:08:17 +0000

    Sync LeetCode submission - Flood Fill (java)

--- my-folder/problems/flood_fill/solution.java
+++ my-folder/problems/flood_fill/solution.java
@@ -1,18 +1,26 @@
 class Solution {
+    int[][] dir = new int[][]{{-1,0}, {1,0},{0,-1},{0,1}};
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        dfs(image, sr, sc, color, image[sr][sc], new boolean[image.length][image[0].length]);
+
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{sr, sc});
+        int ref = image[sr][sc];
+        while(!queue.isEmpty()){
+            int[] t = queue.poll();
+            image[t[0]][t[1]] = color;
+            for(int[] d:dir){
+                int x = d[0]+t[0];
+                int y =  d[1]+t[1];
+                if(isValid(image, x, y, color, ref)){
+                    queue.add(new int[]{x, y});
+                }
+            }
+        }
         return image;
     }
-    
-    public void dfs(int[][] image, int sr, int sc, int color, int prev, boolean[][] visited) {
-        if(sr<0 || sc<0 || sr>=image.length || sc >= image[0].length || image[sr][sc] != prev || visited[sr][sc])
-            return;
-        image[sr][sc] = color;
-        visited[sr][sc] = true;
-        
-        dfs(image, sr+1, sc, color, prev, visited);
-        dfs(image, sr-1, sc, color, prev, visited);
-        dfs(image, sr, sc+1, color, prev, visited);
-        dfs(image, sr, sc-1, color, prev, visited);
+    public boolean isValid(int[][] image, int sr, int sc, int color, int ref){
+        if(sr<0 || sc <0 || sr >= image.length || sc >= image[0].length || image[sr][sc] == color || image[sr][sc] != ref)
+            return false;
+        return true;
     }
 }
