efd908d577dfd5ffb42b8a9ffea3311c37a900ae
Nitish Sharma <nitss007@gmail.com>
2023-01-23 17:52:07 +0000

    Sync LeetCode submission - Find the Town Judge (java)

--- my-folder/problems/find_the_town_judge/solution.java
+++ my-folder/problems/find_the_town_judge/solution.java
@@ -1,41 +1,46 @@
+// class Solution {
+//     public int findJudge(int n, int[][] trust) {
+//         if(trust.length ==0)
+//             return n>1?-1:1;
+//         if(trust.length ==1)
+//             return trust[0][1];
+//         Map<Integer, List<Integer>> map = new HashMap<>();
+
+//         for(int[] t:trust){
+//             if(map.containsKey(t[0]))
+//                 map.remove(t[0]);
+//             else
+//                 map.computeIfAbsent(t[1], ArrayList::new).add(t[0]);
+//         }
+//         for(Map.Entry<Integer, List<Integer>> entry:map.entrySet()){
+//             if(entry.getValue().size() > map.size())
+//                 return entry.getKey();
+
+//         }
+//         return -1;
+//     }
+// }
+
 class Solution {
-	int result=0;
-    public int findJudge(int n, int[][] trust) {
+    public int findJudge(int N, int[][] trust) {
         
-       int[] arr =  new int[n+1];
-       for(int i=0;i<trust.length;i++) {
-           arr[trust[i][0]]--;
-           arr[trust[i][1]]++;
-       }
-       for(int i=1;i<arr.length;i++){
-           if(arr[i]==n-1)
-               return i;
-       } 
-        return -1;
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        /*
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i=0;i<trust.length;i++)
-        {
-            map.put(trust[i][1], map.getOrDefault(trust[i][1], 0)+1);
+        if (trust.length < N - 1) {
+            return -1;
         }
         
-       map.entrySet().stream().filter(s->s.getValue()>=trust.length-1).collect(Collectors.toMap(e->e.getKey(),e->e.getValue())).forEach((k,v)-> result = k);
-       return result==0?-1:result;
-       */
+        int[] indegrees = new int[N + 1];
+        int[] outdegrees = new int[N + 1];
+
+        for (int[] relation : trust) {
+            outdegrees[relation[0]]++;
+            indegrees[relation[1]]++; 
+        }
+
+        for (int i = 1; i <= N; i++) {
+            if (indegrees[i] == N - 1 && outdegrees[i] == 0) {
+                return i;
+            }
+        }
+        return -1;
     }
 }
