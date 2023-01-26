12a50a429cc3cc848c44a2ea9e149d53546bf36c
Nitish Sharma <nitss007@gmail.com>
2023-01-26 13:45:41 +0000

    Sync LeetCode submission - Cheapest Flights Within K Stops (java)

--- my-folder/problems/cheapest_flights_within_k_stops/solution.java
+++ my-folder/problems/cheapest_flights_within_k_stops/solution.java
@@ -2,39 +2,39 @@ class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
         Map<Integer, List<Integer[]>> map = new HashMap<>();
         boolean dstnPresent = false;
-        for(int[] flight: flights){
+        for(int[] flight:flights){
             if(flight[1] == dst)
-                dstnPresent = true;
-            map.computeIfAbsent(flight[0], ArrayList::new).add(new Integer[]{flight[1], flight[2]});
+                dstnPresent = true;            
+            map.computeIfAbsent(flight[0], ArrayList::new). add(new Integer[]{flight[0], flight[1], flight[2]});
         }
         if(map.get(src)==null || !dstnPresent)
             return -1;
-        
-        Queue<int[]> bfs = new LinkedList<>();
-        bfs.add(new int[]{src,0});
 
-        int[] distance = new int[n];
-        Arrays.fill(distance, Integer.MAX_VALUE);
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{src, 0});
+        int[] cost = new int[n];
+        Arrays.fill(cost, Integer.MAX_VALUE);
+        int count = 0;
+        while(k-->=0 && !queue.isEmpty()){
+            int size = queue.size();
+            for(int i=0;i<size;i++){
+                int[] departureAiport = queue.poll();
+                int distanceUptil = departureAiport[1];
 
-        while(k-->=0 && !bfs.isEmpty()){
-            int size = bfs.size();
-            while(size-->0){
-                int[] tmp = bfs.poll();
-                int dist = tmp[1], root = tmp[0];
-                List<Integer[]> adjList = map.get(tmp[0]);
-                if(adjList== null || adjList.size()==0)
+                List<Integer[]> arrivalAirports = map.get(departureAiport[0]);
+
+                if(arrivalAirports == null || arrivalAirports.size()<1)
                     continue;
-                
-                for(Integer[] node: adjList){
-                    int next = node[0], d = node[1];
-                    if(d+dist >= distance[next])
+
+                for(Integer[] arrivalAirport:arrivalAirports){
+                    if(arrivalAirport[2]+distanceUptil> cost[arrivalAirport[1]])
                         continue;
-                    distance[next] = d+dist;
-                    bfs.add(new int[]{next, distance[next]});
+                    cost[arrivalAirport[1]] = Math.min(arrivalAirport[2]+distanceUptil, cost[arrivalAirport[1]]);
+                    queue.add(new int[]{arrivalAirport[1], cost[arrivalAirport[1]]});
                 }
             }
         }
-        return distance[dst] == Integer.MAX_VALUE? -1:distance[dst];
+        return cost[dst] == Integer.MAX_VALUE?-1:cost[dst];
 
     }
 }
