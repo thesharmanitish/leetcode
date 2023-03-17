30a210b221bfce706a85774c4cbb485dfababf01
Nitish Sharma <nitss007@gmail.com>
2023-03-17 03:51:26 +0000

    Sync LeetCode submission - Construct Binary Tree from Inorder and Postorder Traversal (java)

--- my-folder/problems/construct_binary_tree_from_inorder_and_postorder_traversal/solution.java
+++ my-folder/problems/construct_binary_tree_from_inorder_and_postorder_traversal/solution.java
@@ -15,26 +15,29 @@
  */
 class Solution {
     public TreeNode buildTree(int[] inorder, int[] postorder) {
-        return buildTree(inorder,0,inorder.length-1,postorder,0,postorder.length-1);
+        int n = inorder.length-1;
+        return buildTree(inorder, 0, n, postorder, 0, n);
     }
     public TreeNode buildTree(int[] inorder, int istart, int iend, int[] postorder, int pstart, int pend) {
-        int newpstart=0,newiend=0;
-        if (pstart>pend || istart>iend) return null;
-        int i=0;
-        TreeNode root = new TreeNode(postorder[pend]) ;
-        for( i=0;i<=iend;i++){
-            if(inorder[i]==root.val){
-                break;
-            }
-            
-                
-        }
-        /*
-           TreeNode leftchild = buildTreePostIn(inorder, is, ri-1, postorder, ps, ps+leftlen, hm);
-        TreeNode rightchild = buildTreePostIn(inorder,ri+1, ie, postorder, pe-1-rightlen, pe-1, hm);
-        */
-        root.left = buildTree(inorder,istart,i-1,postorder,pstart,pstart+i-istart-1);
-        root.right = buildTree(inorder,i+1,iend,postorder,pend -(iend - i-1)-1,pend-1);
-        return root;
-    }
+           int newpstart=0,newiend=0;
+           if (pstart>pend || istart>iend) 
+                return null;
+           int i=0;
+           
+           if(istart == iend){
+               return new TreeNode(inorder[istart]);
+           }
+           for(i=istart; i<=iend;i++){
+               if(inorder[i] == postorder[pend]){
+                    TreeNode t = new TreeNode(inorder[i]);
+                    t.left = buildTree(inorder, istart, i-1, postorder, pstart, pstart+(i-1-istart));           
+                    t.right = buildTree(inorder, i+1, iend, postorder, pstart+i-istart, pend-1); 
+                    return t;
+               }
+           }
+
+
+
+            return null;          
+    }    
 }
