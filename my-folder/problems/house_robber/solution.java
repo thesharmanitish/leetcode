3816f9026fd9c53669bd90d6b7bbb168316f56a3
Nitish Sharma <nitss007@gmail.com>
2023-03-19 10:33:05 +0000

    Sync LeetCode submission - House Robber (java)

--- my-folder/problems/house_robber/solution.java
+++ my-folder/problems/house_robber/solution.java
@@ -9,9 +9,9 @@ class Solution {
         dp[1] = nums[0];
         dp[2] = nums[1];
 
-        for(int i=3;i<dp.length;i++){
-            dp[i] = Math.max(dp[i-3], dp[i-2])+nums[i-1];
+        for(int i=2;i<nums.length;i++){
+            dp[i+1] = Math.max(dp[i-1], dp[i-2])+nums[i];
         }
-        return Math.max(dp[nums.length], dp[nums.length-1]);
+        return Math.max(dp[nums.length-1], dp[nums.length]);
     }
 }
