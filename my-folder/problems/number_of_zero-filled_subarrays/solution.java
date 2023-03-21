f768a819e594673a6712b63d133ddbeeac4dd4b3
Nitish Sharma <nitss007@gmail.com>
2023-03-21 04:55:18 +0000

    Sync LeetCode submission - Number of Zero-Filled Subarrays (java)

--- my-folder/problems/number_of_zero-filled_subarrays/solution.java
+++ my-folder/problems/number_of_zero-filled_subarrays/solution.java
@@ -1,29 +1,18 @@
 class Solution {
     public long zeroFilledSubarray(int[] nums) {
-//         long res =0;
-        
-//         for (int i = 0, j = 0; i < nums.length; ++i) {
-//             if (nums[i] != 0)
-//                 j = i + 1;
-//              res += i - j + 1;
-//         }      
-//         return res;
-        
-        long count =0, result = 0;
-        
-        for(int i=0;i<nums.length;i++)
-        {
-            if(nums[i]==0)
+        long count = 0, cur = 0, i=0;
+        for(int num:nums){
+            if(num==0){
                 count++;
-            else
-            {
-                result=result+(count*(count+1))/2;
-                count=0;
+                cur+=i;
+                i++;
+            }else{
+                count+=Math.max(cur, 0);
+                cur = 0;
+                i=0;
             }
-            
         }
-        return result+(count*(count+1))/2;
-        
-        
+        count+=Math.max(cur, 0);
+        return count;
     }
 }
