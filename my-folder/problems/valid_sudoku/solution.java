38680f054543f89a79e79769787693d280c39bba
Nitish Sharma <nitss007@gmail.com>
2023-03-23 11:48:20 +0000

    Sync LeetCode submission - Valid Sudoku (java)

--- my-folder/problems/valid_sudoku/solution.java
+++ my-folder/problems/valid_sudoku/solution.java
@@ -1,25 +1,30 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        Set<Character>[] rows = new HashSet[9];
-        Set<Character>[] cols = new HashSet[9];
-        Set<Character>[][] matrix = new HashSet[3][3];
-
-        for(int i=0;i<9;i++){
-            rows[i] = new HashSet<>();
-            for(int j=0;j<9;j++){
+        HashSet<Character>[] rows = new HashSet[9];
+        HashSet<Character>[] cols = new HashSet[9];
+        
+        HashSet<Character>[][] matrix = new HashSet[3][3];
+        
+        for(int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                
                 if(board[i][j] =='.')
-                    continue;                  
-                if(matrix[i/3][j/3] == null)
-                    matrix[i/3][j/3] = new HashSet<>();                
-
-                if(cols[j]==null)
-                    cols[j] = new HashSet<>();      
-
-                if(!cols[j].add(board[i][j]) || !rows[i].add(board[i][j]) || !matrix[i/3][j/3].add(board[i][j]))
+                    continue; 
+                if(rows[i] ==null)
+                    rows[i] = new HashSet<>();
+                if(cols[j] ==null)
+                    cols[j] = new HashSet<>();
+                if(matrix[i/3][j/3] ==null)
+                    matrix[i/3][j/3] = new HashSet<>();
+                
+                if(rows[i].contains(board[i][j]) || cols[j].contains(board[i][j]) || 
+                   matrix[i/3][j/3].contains(board[i][j]))
                     return false;
+                rows[i].add(board[i][j]);
+                cols[j].add(board[i][j]);
+                matrix[i/3][j/3].add(board[i][j]);
             }
         }
-               
-        return true;
+        return true;                             
     }
 }
