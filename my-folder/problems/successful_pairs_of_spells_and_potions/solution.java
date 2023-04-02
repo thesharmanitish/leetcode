b585753d39c269e2399f8fd6de530e47e20d0d9f
Nitish Sharma <nitss007@gmail.com>
2023-04-02 13:11:56 +0000

    Sync LeetCode submission - Successful Pairs of Spells and Potions (java)

--- my-folder/problems/successful_pairs_of_spells_and_potions/solution.java
+++ my-folder/problems/successful_pairs_of_spells_and_potions/solution.java
@@ -13,3 +13,31 @@ class Solution {
 		return spells;
 	}
 }
+
+// class Solution {
+//     public int[] successfulPairs(int[] spells, int[] potions, long success) {
+//         Arrays.sort(potions);
+//         int x = 0;
+//         int[] count = new int[spells.length];
+//         for(int i=spells.length-1;i>=0;i--){
+//              count[i] = potions.length - bsearch(potions, spells[i], success);
+//         }
+//         return count;
+//     }
+
+//     public int bsearch(int[] potions, int spell, long success) {
+//         int left = 0, right = potions.length;
+
+//         while(left<right){
+//             int mid = left +(right-left)/2;
+//             long x = (long)spell * potions[mid];
+//             if(x< success){
+//                 left = mid+1;
+//             }else{
+//                 right = mid;
+//             }
+//         }
+//         return left==potions.length?potions.length:left;
+
+//     }
+// }
