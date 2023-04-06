0f72928f3b530018d7aa74b6f0563cde3c0a5485
Nitish Sharma <nitss007@gmail.com>
2023-04-06 18:50:31 +0000

    Sync LeetCode submission - Number of Closed Islands (java)

--- my-folder/problems/number_of_closed_islands/solution.java
+++ my-folder/problems/number_of_closed_islands/solution.java
@@ -1,37 +1,33 @@
 class Solution {
     public int closedIsland(int[][] grid) {
-        int count = 0;
+        
         for(int i=0;i<grid.length;i++){
-            if(grid[0][i] ==0 || grid[grid.length-1][i]==0)
-                floodfill(grid, 0, i);
-            if(grid[i][0] ==0 || grid[i][grid[0].length-1]==0)
-                floodfill(grid,i, 0);
-            if(grid[grid.length-1][i]==0)
-                floodfill(grid, grid.length-1, i);
-            if(grid[i][grid[0].length-1]==0)
-                floodfill(grid, i, grid[0].length-1);            
+                fill(grid, i, 0);
+                fill(grid, i, grid[0].length-1);
         }
+        for(int i=0;i<grid[0].length;i++){
+                fill(grid, 0, i);
+                fill(grid, grid.length-1, i);
+        }
+        int count = 0;
         for(int i=1;i<grid.length-1;i++){
             for(int j=1;j<grid[0].length-1;j++){
                 if(grid[i][j] ==0){
-                    floodfill(grid,i, j);
-                    count +=1;
+                    fill(grid, i ,j);
+                    count++;
                 }
+
             }
         }
-        
-        
         return count;
     }
-    public void floodfill(int[][] grid, int i, int j) {
-        if(i<0 || j<0 || i>= grid.length || j>= grid[0].length || grid[i][j] ==1)
-            return ;
-        
+    public void fill(int[][] grid, int i, int j) {
+        if(i<0 || j<0 || i>= grid.length || j>=grid[0].length || grid[i][j] ==1)
+            return;
         grid[i][j] = 1;
-        
-        floodfill(grid, i, j+1);
-        floodfill(grid, i, j-1);
-        floodfill(grid, i+1, j);
-        floodfill(grid, i-1, j);
-    }   
+        int[] dir = new int[]{0, 1, 0, -1, 0};
+        for(int k=0;k<dir.length-1;k++){
+            fill(grid, i+dir[k], j+dir[k+1]);
+        }
+    }
 }
