336d4c93492dc3a7ec725f24be4697d415ae04fd
Nitish Sharma <nitss007@gmail.com>
2023-04-07 09:58:00 +0000

    Sync LeetCode submission - Number of Enclaves (java)

---
+++ my-folder/problems/number_of_enclaves/solution.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numEnclaves(int[][] grid) {
+        for(int i=0;i<grid.length;i++){
+                fill(grid, i, 0);
+                fill(grid, i, grid[0].length-1);
+        }
+        for(int i=0;i<grid[0].length;i++){
+                fill(grid, 0, i);
+                fill(grid, grid.length-1, i);
+        }
+        int count = 0;
+        for(int i=1;i<grid.length-1;i++){
+            for(int j=1;j<grid[0].length-1;j++){
+                if(grid[i][j] == 1){
+                    // fill(grid, i ,j);
+                    count++;
+                }
+
+            }
+        }
+        return count;        
+    }
+    public void fill(int[][] grid, int i, int j) {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j] ==0)
+            return;
+        
+        grid[i][j] = 0;
+
+        int[] dir = new int[]{0,1,0,-1,0};
+
+        for(int k=0;k<4;k++){
+            fill(grid, i+dir[k], j+dir[k+1]);
+        }
+
+    }
+}
